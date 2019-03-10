e9bc2f68ab2cee5678766b2e9f24b8c780b6a740
Daniel Sjoberg <sjobergd@mskcc.org>
2019-03-10 14:06:17 -0400

    put all print methods in one Rd file

--- R/print.R
+++ R/print.R
@@ -1,14 +1,17 @@
-#' Print gtsummary objects
+#' print and knit_print methods for gtsummary objects
 #'
-#' @param x object of class `tbl_summary` object from \code{\link{tbl_summary}} function
-#' @param ... further arguments passed to or from other methods.
+#' @name print_gtsummary
+#' @param x an object created using gtsummary functions
+#' @param ... not used
+#' @author Daniel Sjoberg
+#' @seealso \link{tbl_summary} \link{tbl_regression} \link{tbl_uvregression}
+NULL
+
+#' @rdname print_gtsummary
 #' @export
 print.tbl_summary <- function(x, ...) as_gt(x) %>% print()
 
-#' Print gtsummary objects
-#'
-#' @param x object of class `tbl_summary` object from \code{\link{tbl_summary}} function
-#' @param ... further arguments passed to or from other methods.
+#' @rdname print_gtsummary
 #' @export
 knit_print.tbl_summary <- function(x, ...) {
   if ("word_document" %in% rmarkdown::all_output_formats(knitr::current_input())) {
@@ -17,30 +20,18 @@ knit_print.tbl_summary <- function(x, ...) {
   as_gt(x) %>% knitr::knit_print()
 }
 
-#' Print gtsummary objects
-#'
-#' @param x object of class `tbl_regression` object from \code{\link{tbl_regression}} function
-#' @param ... further arguments passed to or from other methods.
+#' @rdname print_gtsummary
 #' @export
 print.tbl_regression <- print.tbl_summary
 
-#' Print gtsummary objects
-#'
-#' @param x object of class `tbl_regression` object from \code{\link{tbl_regression}} function
-#' @param ... further arguments passed to or from other methods.
+#' @rdname print_gtsummary
 #' @export
 knit_print.tbl_regression <- knit_print.tbl_summary
 
-#' Print gtsummary objects
-#'
-#' @param x object of class `tbl_uvregression` object from \code{\link{tbl_uvregression}} function
-#' @param ... further arguments passed to or from other methods.
+#' @rdname print_gtsummary
 #' @export
 print.tbl_uvregression <- print.tbl_summary
 
-#' Print gtsummary objects
-#'
-#' @param x object of class `tbl_uvregression` object from \code{\link{tbl_uvregression}} function
-#' @param ... further arguments passed to or from other methods.
+#' @rdname print_gtsummary
 #' @export
 knit_print.tbl_uvregression <- knit_print.tbl_summary
