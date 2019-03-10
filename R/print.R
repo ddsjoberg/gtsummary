da724ab09ae075eec6a0a3fe1955ffc95ec9b9db
Daniel Sjoberg <sjobergd@mskcc.org>
2019-03-10 11:54:18 -0400

    #21 added warning when output is unsupport "word_document"

--- R/print.R
+++ R/print.R
@@ -10,7 +10,12 @@ print.tbl_summary <- function(x, ...) as_gt(x) %>% print()
 #' @param x object of class `tbl_summary` object from \code{\link{tbl_summary}} function
 #' @param ... further arguments passed to or from other methods.
 #' @export
-knit_print.tbl_summary <- function(x, ...) as_gt(x) %>% knitr::knit_print()
+knit_print.tbl_summary <- function(x, ...) {
+  if ("word_document" %in% rmarkdown::all_output_formats(knitr::current_input())) {
+    warning("Output 'word_document' is not suported by the {gt} package.  Try 'output: rtf_document' for output compatible with MS Word.")
+  }
+  as_gt(x) %>% knitr::knit_print()
+}
 
 #' Print gtsummary objects
 #'
