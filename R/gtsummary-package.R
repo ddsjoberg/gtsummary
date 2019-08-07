77d7c7e196cce1c065f19bcba1d7e1311293fc6f
Daniel Sjoberg <danield.sjoberg@gmail.com>
2019-08-07 07:30:49 -0400

    Update gtsummary-package.R

--- R/gtsummary-package.R
+++ R/gtsummary-package.R
@@ -10,6 +10,7 @@
 # allowing for the use of the dot when piping
 utils::globalVariables(c("."))
 
+# THIS IS FROM PR #195, won't be required when tidyr >=1.0.0 is deps
 tidyr_has_legacy_nest <- function() {
   exists("unnest_legacy", asNamespace("tidyr"))
 }
