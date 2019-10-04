c523e80f248051ec163811b056baaa33c47b62e7
Daniel D. Sjoberg <danield.sjoberg@gmail.com>
2019-10-04 13:02:35 -0400

    Update test-add_n.R

--- tests/testthat/test-add_n.R
+++ tests/testthat/test-add_n.R
@@ -13,8 +13,20 @@ test_that("no errors/warnings with standard use", {
   expect_warning(t1 %>% add_n(last = TRUE), NA)
   expect_warning(t2 %>% add_n(last = TRUE), NA)
 
+  expect_error(t1 %>% add_n(statistic = "{N}{n}{n_miss}{p}{p_miss}",
+                            footnote = TRUE), NA)
+  expect_error(t2 %>% add_n(statistic = "{N}{n}{n_miss}{p}{p_miss}",
+                            footnote = TRUE), NA)
+  expect_warning(t1 %>% add_n(statistic = "{N}{n}{n_miss}{p}{p_miss}",
+                              footnote = TRUE), NA)
+  expect_warning(t2 %>% add_n(statistic = "{N}{n}{n_miss}{p}{p_miss}",
+                              footnote = TRUE), NA)
+
+})
+
+
+test_that("testing deprecated missing argument", {
+  expect_warning(t1 %>% add_n(missing = TRUE), "*")
   expect_error(t1 %>% add_n(missing = TRUE), NA)
   expect_error(t2 %>% add_n(missing = TRUE), NA)
-  expect_warning(t1 %>% add_n(missing = TRUE), NA)
-  expect_warning(t2 %>% add_n(missing = TRUE), NA)
 })
