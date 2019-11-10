3ec67832663c77f694388c9e2fcc5dedbd122b6e
Daniel Sjoberg <danield.sjoberg@gmail.com>
2019-11-10 08:11:27 -0500

    delete revdeps

--- revdep/library/gtsummary/new/Rcpp/doc/Rcpp-extending.R
+++
@@ -1,6 +0,0 @@
-## ------------------------------------------------------------------------
-# Run sourceCpp compilation to include file
-# Rcpp::sourceCpp(file= "code.cpp")
-input <- list( x = seq(1, 10, by = 0.5) )
-fx(input)
-
