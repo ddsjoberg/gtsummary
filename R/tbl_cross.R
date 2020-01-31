a93ff6442925a40442b4dc9768accc999f1fc657
karissawhiting <karissa.whiting@gmail.com>
2020-01-31 17:27:59 -0500

    simplify function

--- R/tbl_cross.R
+++ R/tbl_cross.R
@@ -1,3 +1,4 @@
+
 #' Create a cross table of summary statistics
 #'
 #' Wrapper for [tbl_summary] to cross tabulate two variables
@@ -38,11 +39,12 @@
 tbl_cross <- function(data,
                       row = NULL,
                       col = NULL,
-                      percent = c("none", "column", "row", "cell"),
-                      statistic = ifelse(percent == "none", "{n}", "{n} ({p}%)"),
+                      percent = c(NULL, "column", "row", "cell"),
+                      statistic = NULL,
                       label = NULL,
                       missing = c("ifany", "always", "no"),
-                      ) {
+                      missing_text = "Unknown") {
+
   row <- var_input_to_string(
     data = data,
     select_input = !!rlang::enquo(row),
@@ -61,114 +63,69 @@ tbl_cross <- function(data,
   missing <- match.arg(missing)
   percent <- match.arg(percent)
 
+
   # if no x and y provided, default to first two columns of data ---------------
   if (is.null(row) & is.null(col)) {
     row <- names(data[, 1])
     col <- names(data[, 2])
   }
 
-  if (is.null(row)) row <- names(data[, 1])
-  if (is.null(col)) col <- names(data[, 1])
+  row <- row %||% names(select(data, -col))[1]
+  col <- col %||% names(select(data, -row))[1]
 
+  # create column for tabulating column total
   data <- data %>%
-    dplyr::select(row, col)
-
-  # set labels -----------------------------------------------------------------
-
-  # check for labels
-  lbls <- map(data, ~ attr(.x, "label"))
-
-  # overide labels if label argument exists
-  if (!is.null(label)) {
-    lbl_replace <- tidyselect_to_list(data,
-      label,
-      arg_name = "label"
-    )
-
-    missing_lbls <- setdiff(names(lbls), names(lbl_replace))
-    lbls <- c(lbl_replace, lbls[missing_lbls])
-  }
-
-  # if no labels and no label arg, use column names (needed for col tab_spanner)
-  lbls <- imap(lbls, ~ ifelse(is.null(.x), .y, .x))
-
-  data <- data %>%
-    set_names("row", "col")
-
-  # omit missing data, or factorize Unknown level ------------------------------
-  if (missing == "no") {
-    if (sum(is.na(data$row)) > 0) {
-      message(glue::glue(
-        "{sum(is.na(data$row))} observations missing `{row}` have been removed."
-      ))
-    }
-
-    if (sum(is.na(data$col)) > 0) {
-      message(glue::glue(
-        "{sum(is.na(data$col))} observations missing `{col}` have been removed."
-      ))
-    }
-    data <- data %>%
-      na.omit()
-
-  } else if (missing == "ifany") {
-    data <- data %>%
-      mutate(col = forcats::fct_explicit_na(col, "Unknown"))
-  } else if (missing == "always") {
-    data <- data %>%
-      mutate(col = case_when(
-        is.na(col) ~ "Unknown",
-        TRUE ~ as.character(col)
-      )) %>%
-      mutate(col = forcats::fct_expand(col, "Unknown"))
-  }
-
-  # create tbl_summary() ---------------
-
-  # add column to compute totals
-  data <- data %>%
+    dplyr::select(row, col) %>%
     mutate(Total = 1)
 
-  # get label of by variable for use in tab_spanner
-  # col_label <- labels[[col]]
-  # if (is.null(col_label)) col_label <- col
+  # get column label and user-passed labels ------------------------------------
+  label <- tidyselect_to_list(data, label) %||% NULL
+  col_label <- label[[col]] %||% attr(data[[col]], "label") %||% col
 
+  # statistic argument ---------------------------------------------------------
+
+  # if no user-defined stat, default to {n} if percent is none
+  statistic <- statistic %||% ifelse(percent == "none", "{n}", "{n} ({p}%)")
+  statistic_list <- rep(statistic, 3) %>% as.list()
+  names(statistic_list) <- names(data)
+
+
+  # omit missing data, or factorize missing level ------------------------------
+  lbls <- purrr::map(data, ~ attr(.x, "label"))
+
+  data <- data %>%
+    mutate_at(vars(row, col), ~as.factor(.x)) %>%
+    mutate_at(vars(row,col),
+              ~switch(
+                missing,
+                "no" = .,
+                "ifany" = forcats::fct_explicit_na(., missing_text),
+                "always" = forcats::fct_explicit_na(., missing_text) %>%
+                  forcats::fct_expand(missing_text)))
+
+  # re-applying labels
+  for (i in names(lbls)) {
+    attr(data[[i]], "label") <- lbls[[i]] }
 
   x <- data %>%
     tbl_summary(
       by = col,
-      statistic = list(
-        row = statistic,
-        Total = statistic
-      ),
-      type = list(
-        row = "categorical",
-        Total = "dichotomous"
-      ),
-      missing = missing,
-      label = list(
-        row = labels[[row]],
-        Total = "Total"
-      )
-    ) %>%
+      statistic = statistic_list,
+      percent = percent,
+      label = label,
+      missing_text = missing_text) %>%
     add_overall(last = TRUE) %>%
     bold_labels() %>%
     modify_header(
       stat_by = "{level}",
-      stat_0 = " "
-    )
-
-
-  x$table_body <- x$table_body %>%
-    filter(!(variable == "Total" & label == "Unknown"))
-
+      stat_0 = " ")
 
   # gt function calls ------------------------------------------------------------
   # quoting returns an expression to be evaluated later
   x$gt_calls[["tab_spanner"]] <-
     glue(
       "gt::tab_spanner(",
-      "label = gt::md('**{labels[[col]]}**'), ",
+      "label = gt::md('**{col_label}**'), ",
       "columns = contains('stat_')) %>%",
       "gt::tab_spanner(label = gt::md('**Total**'), ",
       "columns = vars(stat_0))"
@@ -178,36 +135,3 @@ tbl_cross <- function(data,
 
 }
 
-attr(starwars, "label")
-r <- tbl_cross(starwars, row = gender, col = hair_color, missing = "always")
-r
-
-tbl_cross(trial,
-  row = response, col = trt,
-  label = list(trt = "XX"),
-  missing = "ifany"
-)
-r
-
-r <- tbl_cross(trial, row = response, col = trt, missing = "no")
-r
-
-r <- tbl_cross(trial, row = grade, col = trt, missing = "no")
-# r$gt_calls
-r
-
-# Iris
-iris[1, "Species"] <- NA_character_
-
-r <- tbl_cross(iris, row = Species, col = Species, missing = "always")
-r
-
-r <- tbl_cross(trial, row = response, col = trt, missing = "ifany")
-r
-
-r <- tbl_cross(trial, row = response, col = trt, missing = "no")
-r
-
-r <- tbl_cross(trial, row = grade, col = trt, missing = "no")
-# r$gt_calls
-r
