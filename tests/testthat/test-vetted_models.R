b914603bd97e2b14edb26e5f2153bbaa5452c2ab
Daniel Sjoberg <danield.sjoberg@gmail.com>
2020-02-24 18:56:20 -0500

    Update test-vetted_models.R

--- tests/testthat/test-vetted_models.R
+++ tests/testthat/test-vetted_models.R
@@ -21,8 +21,8 @@ set.seed(23433)
 
 # function to pull estimates from tbl_regression object
 # input gt object, output vector of coefficients
-coefs_in_gt <- function(object) {
-  object$table_body %>%
+coefs_in_gt <- function(x) {
+  x$table_body %>%
     dplyr::pull(estimate) %>%
     na.omit() %>%
     as.vector()
