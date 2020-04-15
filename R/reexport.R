0fec98a28ed6c5d9fbbc9712ffd100223257052f
Daniel Sjoberg <danield.sjoberg@gmail.com>
2020-04-15 10:24:04 -0400

    made as_flextable a generic #445

--- R/reexport.R
+++ R/reexport.R
@@ -8,6 +8,11 @@ magrittr::`%>%`
 #' @importFrom tibble as_tibble
 tibble::as_tibble
 
+# flextable --------------------------------------------------------------------
+#' @export
+#' @importFrom flextable as_flextable
+flextable::as_flextable
+
 # knitr ------------------------------------------------------------------------
 #' @export
 #' @importFrom knitr knit_print
