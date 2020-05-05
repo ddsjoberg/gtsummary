083e9c5aa1ab810aa394c0550c8b19fe2202a43d
Daniel Sjoberg <danield.sjoberg@gmail.com>
2020-05-04 21:58:02 -0400

    error/testing updates

--- R/utils-add_p.R
+++ R/utils-add_p.R
@@ -97,8 +97,7 @@ assign_test_one <- function(data, var, var_summary_type, by_var, test, group, en
 
   # unless by_var has >2 levels, then return NA with a message
   if (!is.null(group) & length(unique(data[[by_var]])) > 2) {
-    message(paste0(var, ": P-value calculation for correlated data when by variables have >2 levels is not currently supported"))
-    return(NULL)
+    stop(glue("{var}: There is not default test for correlated data when `by=` variable has >2 levels."))
   }
 
   # for continuous data, default to non-parametric tests
--- tests/testthat/test-add_p.R
+++ tests/testthat/test-add_p.R
@@ -2,6 +2,11 @@ context("test-add_p.tbl_summary")
 
 test_that("add_p creates output without error/warning", {
   expect_error(
+    tbl_summary(trial, by = grade) %>% add_p(),
+    NA
+  )
+
+  expect_error(
     tbl_summary(mtcars, by = am) %>% add_p(),
     NA
   )
@@ -33,15 +38,27 @@ test_that("add_p creates output without error/warning", {
 
   expect_error(
     tbl_summary(trial, by = trt, include = -response) %>%
-      add_p(test = everything() ~ "lme4", group = response),
+      add_p(group = response),
     NA
+  )
+
+  expect_message(
+    tbl_summary(trial, by = trt) %>%
+      add_p(test = everything() ~ "lme4", group = response),
+    "*"
   )
 })
 
-test_that("add_p creates errors when non-function in input", {
+test_that("add_p creates errors with bad args", {
   expect_error(
     tbl_summary(mtcars, by = am) %>%
       add_p(pvalue_fun = mtcars),
+    "*"
+  )
+
+  expect_error(
+    tbl_summary(trial, by = grade, include = -response) %>%
+      add_p(group = response),
     "*"
   )
 })
