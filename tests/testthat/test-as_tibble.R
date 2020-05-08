fbd8890ac7a97f4cd73d8427f538f7d3b624384e
Daniel Sjoberg <danield.sjoberg@gmail.com>
2020-05-07 21:25:43 -0400

    bold_p() bug for large p-values (#491)
    * #489 bolding p-value bug fix
    
    * Update test-as_tibble.R
    
    * Update NEWS.md
    
    * Update NEWS.md
    
    * fixed italic code + added check to unit tests
    
    * Update test-as_tibble.R
    
    * Increment version
    
    * Update R-CMD-check.yaml
    
    Co-authored-by: edrill <esther.drill@gmail.com>

--- tests/testthat/test-as_tibble.R
+++ tests/testthat/test-as_tibble.R
@@ -1,9 +1,17 @@
 context("test-as_tibble")
 
-t1 <- trial %>% tbl_summary()
+t1 <- trial %>%
+  tbl_summary() %>%
+  bold_labels() %>%
+  italicize_levels()
+
 t2 <-
   glm(response ~ age + grade, trial, family = binomial(link = "logit")) %>%
-  tbl_regression(exponentiate = TRUE)
+  tbl_regression(exponentiate = TRUE) %>%
+  bold_labels() %>%
+  italicize_levels() %>%
+  bold_p()
+
 t3 <-
   tbl_uvregression(
     trial %>% dplyr::select(response, age, grade),
@@ -11,7 +19,10 @@ t3 <-
     y = response,
     method.args = list(family = binomial),
     exponentiate = TRUE
-  )
+  ) %>%
+  bold_levels() %>%
+  italicize_labels() %>%
+  bold_p()
 
 test_that("as_tibble works with standard use", {
   expect_error(as_tibble(t1), NA)
@@ -22,4 +33,31 @@ test_that("as_tibble works with standard use", {
   expect_warning(as_tibble(t2), NA)
   expect_error(as_tibble(t3), NA)
   expect_warning(as_tibble(t3), NA)
+})
+
+
+test_that("as_tibble works with bold_p()", {
+  bold_p_data = data.frame(
+    x = c(rep("YES",10), rep("NO",10)),
+    y = c(rep("YES",9), rep("NO", 11)),
+    a = c(rep(c(rep("YES",5), rep("NO",5)))),
+    b = c(rep("YES",4), rep("NO", 16))
+  )
+
+  # checking that the low pvalue is bolded, and the other two are not
+  expect_equal(
+    bold_p_data %>%
+      tbl_summary(by = x) %>%
+      add_p %>%
+      bold_labels() %>%
+      bold_p() %>%
+      as_tibble(col_labels = FALSE) %>%
+      pull(p.value),
+    c("__<0.001__", ">0.9", "0.087")
+  )
+
+  expect_equal(
+    as_tibble(t2, col_labels = FALSE)$label,
+    c("__Age, yrs__", "__Grade__", "_I_", "_II_", "_III_")
+  )
 })
