a154cd72b26752490166f03691d196bd50301e0c
Daniel D. Sjoberg <danield.sjoberg@gmail.com>
2020-06-29 14:31:03 -0400

    reexports update

--- R/reexport.R
+++ R/reexport.R
@@ -1,3 +1,8 @@
+# magrittr ---------------------------------------------------------------------
+#' @export
+#' @importFrom magrittr %>%
+magrittr::`%>%`
+
 # tibble -----------------------------------------------------------------------
 #' @export
 #' @importFrom tibble as_tibble
@@ -16,3 +21,44 @@ dplyr::vars
 #' @importFrom dplyr select
 #' @export
 dplyr::select
+
+# tidyselect -------------------------------------------------------------------
+#' @importFrom tidyselect starts_with
+#' @export
+tidyselect::starts_with
+
+#' @importFrom tidyselect ends_with
+#' @export
+tidyselect::ends_with
+
+#' @importFrom tidyselect contains
+#' @export
+tidyselect::contains
+
+#' @importFrom tidyselect matches
+#' @export
+tidyselect::matches
+
+#' @importFrom tidyselect num_range
+#' @export
+tidyselect::num_range
+
+#' @importFrom tidyselect all_of
+#' @export
+tidyselect::all_of
+
+#' @importFrom tidyselect any_of
+#' @export
+tidyselect::any_of
+
+#' @importFrom tidyselect everything
+#' @export
+tidyselect::everything
+
+#' @importFrom tidyselect last_col
+#' @export
+tidyselect::last_col
+
+#' @importFrom tidyselect one_of
+#' @export
+tidyselect::one_of
