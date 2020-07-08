994b8a8aa3e63da6ef0bf0f0d4442fe58b975b22
Sjoberg <SjobergD@mskcc.org>
2020-07-07 21:26:53 -0400

    updated to use style_number in background

--- R/style_pvalue.R
+++ R/style_pvalue.R
@@ -1,10 +1,11 @@
-#' Style p-values to be displayed in tables or text
+#' Style p-values
 #'
 #' @param x Numeric vector of p-values.
-#' @param digits Number of digits large p-values are rounded. Must be 1 or 2.
+#' @param digits Number of digits large p-values are rounded. Must be 1, 2, or 3.
 #' Default is 1.
 #' @param prepend_p Logical. Should 'p=' be prepended to formatted p-value.
 #' Default is `FALSE`
+#' @inheritParams style_number
 #' @export
 #' @return A character vector of styled p-values
 #' @family style tools
@@ -17,36 +18,63 @@
 #' )
 #' style_pvalue(pvals)
 #' style_pvalue(pvals, digits = 2, prepend_p = TRUE)
-style_pvalue <- function(x, digits = 1, prepend_p = FALSE) {
+style_pvalue <- function(x, digits = 1, prepend_p = FALSE,
+                         big.mark = NULL, decimal.mark = NULL, ...) {
+  # setting defaults -----------------------------------------------------------
+  big.mark <- big.mark %||% get_theme_element("style_number-arg:big.mark", default = ",")
+  decimal.mark <- decimal.mark %||% get_theme_element("style_number-arg:decimal.mark", default = ".")
 
-  # rounding large pvalues to 2 digits
-  if (digits == 2) {
+  # rounding large p-values to 1 digits
+  if (digits == 1) {
     p_fmt <-
       case_when(
         x > 1 ~ NA_character_,
         x < 0 ~ NA_character_,
-        x > 0.99 ~ ">0.99",
-        round(x, 2) >= 0.1 ~ sprintf("%.2f", x),
-        x >= 0.001 ~ sprintf("%.3f", x),
-        x < 0.001 ~ "<0.001"
+        x > 0.9 ~ paste0(">", style_number(x = 0.9, digits = 1, big.mark = big.mark,
+                                            decimal.mark = decimal.mark, ...)),
+        round(x, 1) >= 0.2 ~ style_number(x, digits = 1, big.mark = big.mark,
+                                          decimal.mark = decimal.mark, ...),
+        round(x, 2) >= 0.1 ~ style_number(x, digits = 2, big.mark = big.mark,
+                                          decimal.mark = decimal.mark, ...),
+        x >= 0.001 ~ style_number(x, digits = 3, big.mark = big.mark,
+                                  decimal.mark = decimal.mark, ...),
+        x < 0.001 ~ paste0("<", style_number(x = 0.001, digits = 3, big.mark = big.mark,
+                                             decimal.mark = decimal.mark, ...))
+      )
+  }
+  # rounding large p-values to 2 digits
+  else if (digits == 2) {
+    p_fmt <-
+      case_when(
+        x > 1 ~ NA_character_,
+        x < 0 ~ NA_character_,
+        x > 0.99 ~ paste0(">", style_number(x = 0.99, digits = 2, big.mark = big.mark,
+                                            decimal.mark = decimal.mark, ...)),
+        round(x, 2) >= 0.1 ~ style_number(x, digits = 2, big.mark = big.mark,
+                                          decimal.mark = decimal.mark, ...),
+        x >= 0.001 ~ style_number(x, digits = 3, big.mark = big.mark,
+                                  decimal.mark = decimal.mark, ...),
+        x < 0.001 ~ paste0("<", style_number(x = 0.001, digits = 3, big.mark = big.mark,
+                                             decimal.mark = decimal.mark, ...))
       )
   }
 
-  # rounding large pvalues to 1 digit
-  else if (digits == 1) {
+  # rounding large pvalues to 3 digit
+  else if (digits == 3) {
     p_fmt <-
       case_when(
         x > 1 ~ NA_character_,
         x < 0 ~ NA_character_,
-        x > 0.9 ~ ">0.9",
-        round(x, 1) >= 0.2 ~ sprintf("%.1f", x),
-        round(x, 2) >= 0.1 ~ sprintf("%.2f", x),
-        x >= 0.001 ~ sprintf("%.3f", x),
-        x < 0.001 ~ "<0.001"
+        x > 0.999 ~ paste0(">", style_number(x = 0.999, digits = 3, big.mark = big.mark,
+                                            decimal.mark = decimal.mark, ...)),
+        x >= 0.001 ~ style_number(x, digits = 3, big.mark = big.mark,
+                                  decimal.mark = decimal.mark, ...),
+        x < 0.001 ~ paste0("<", style_number(x = 0.001, digits = 3, big.mark = big.mark,
+                                             decimal.mark = decimal.mark, ...))
       )
   }
   else {
-    stop("'digits' argument must be 1 or 2.")
+    stop("The `digits=` argument must be 1, 2, or 3.")
   }
 
   # prepending a p = in front of value
