6e0c8976c9723fd6ee0ae5b7efe71869aec6ed6b
ddsjoberg <danield.sjoberg@gmail.com>
2020-07-17 11:58:28 -0400

    testthat additions

--- tests/testthat/test-set_gtsummary_theme.R
+++ tests/testthat/test-set_gtsummary_theme.R
@@ -20,10 +20,19 @@ test_that("setting themes", {
 
   expect_error(
     lm(age ~ marker + grade, trial) %>%
-      tbl_regression(),
+      tbl_regression() %>%
+      as_gt(),
     NA)
 
   expect_error(
     reset_gtsummary_theme(),
     NA)
+
+  expect_error(
+    set_gtsummary_theme(list("not_a_theme_element" = 0)),
+    "*")
+
+  expect_error(
+    get_theme_element("not_a_theme_element"),
+    "*")
 })
