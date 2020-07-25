18978d6bceeded35098f96dfdfe47b409a0053b6
ddsjoberg <danield.sjoberg@gmail.com>
2020-07-25 11:23:05 -0400

    deprecating as_flextable, updating deprecate_warn to stop for <= v1.2.0

--- R/deprecated.R
+++ R/deprecated.R
@@ -12,14 +12,14 @@ NULL
 #' @rdname deprecated
 #' @export
 add_comparison <- function(...) {
-  lifecycle::deprecate_warn("1.1.0", "gtsummary::add_comparison()", "add_p()")
+  lifecycle::deprecate_stop("1.1.0", "gtsummary::add_comparison()", "add_p()")
   add_p(...)
 }
 
 #' @rdname deprecated
 #' @export
 add_global <- function(...) {
-  lifecycle::deprecate_warn("1.1.0", "gtsummary::add_global()", "add_global_p()")
+  lifecycle::deprecate_stop("1.1.0", "gtsummary::add_global()", "add_global_p()")
   add_global_p(...)
 }
 
@@ -28,39 +28,39 @@ add_global <- function(...) {
 #' @export
 
 tab_style_bold_p <- function(...) {
-  lifecycle::deprecate_warn("1.2.0", "gtsummary::tab_style_bold_p()", "bold_p()")
+  lifecycle::deprecate_stop("1.2.0", "gtsummary::tab_style_bold_p()", "bold_p()")
   bold_p(...)
 }
 
 #' @rdname deprecated
 #' @export
 tab_style_bold_labels <- function(...) {
-  lifecycle::deprecate_warn("1.2.0", "gtsummary::tab_style_bold_labels()", "bold_labels()")
+  lifecycle::deprecate_stop("1.2.0", "gtsummary::tab_style_bold_labels()", "bold_labels()")
   bold_labels(...)
 }
 
 #' @rdname deprecated
 #' @export
 tab_style_italicize_levels <- function(...) {
-  lifecycle::deprecate_warn("1.2.0", "gtsummary::tab_style_italicize_levels()", "italicize_levels()")
+  lifecycle::deprecate_stop("1.2.0", "gtsummary::tab_style_italicize_levels()", "italicize_levels()")
   italicize_levels(...)
 }
 
 #' @rdname deprecated
 #' @export
 tab_style_italicize_labels <- function(...) {
-  lifecycle::deprecate_warn("1.2.0", "gtsummary::tab_style_italicize_labels()", "italicize_labels()")
+  lifecycle::deprecate_stop("1.2.0", "gtsummary::tab_style_italicize_labels()", "italicize_labels()")
   italicize_labels(...)
 }
 
 #' @rdname deprecated
 #' @export
 tab_style_bold_levels <- function(...) {
-  lifecycle::deprecate_warn("1.2.0", "gtsummary::tab_style_bold_levels()", "bold_levels()")
+  lifecycle::deprecate_stop("1.2.0", "gtsummary::tab_style_bold_levels()", "bold_levels()")
   bold_levels(...)
 }
 
-# v1.1.5 -----------------------------------------------------------------------
+# v1.2.5 -----------------------------------------------------------------------
 #' @rdname deprecated
 #' @export
 tbl_summary_ <- function(...) {
@@ -76,15 +76,36 @@ add_p_ <- function(...) {
 }
 
 # v1.3.3 -----------------------------------------------------------------------
-as_flextable.gtsummary <- function(x, include = everything(), return_calls = FALSE,
-                                   strip_md_bold = TRUE, group_header = NULL, ...) {
-  # soft deprecated on 2020-07-22 ----------------------------------------------
-  lifecycle::deprecate_warn("1.3.3",
-                            "gtsummary::as_flextable.gtsummary()",
-                            "as_flex_table()")
+as_flextable <- function(...) {
+  lifecycle::deprecate_warn(
+    "1.3.3", "gtsummary::as_flextable()", "as_flex_table()",
+    details = paste(
+      "The `as_flextable()` function graduated",
+      "from 'Experimental' status in v1.3.3. The updated function has",
+      "a new name to avoid name conflicts with {flextable}",
+      "and the ascetics of resulting tables have been improved."
+    ) %>%
+      stringr::str_wrap()
+  )
 
   # passing args to `as_flex_table()` ------------------------------------------
-  rlang::expr(as_flex_table(!!!as.list(environment()))) %>%
-    rlang::eval_tidy()
+  as_flex_table(...)
+}
+
+as_flextable.gtsummary <- function(...) {
+  # soft deprecated on 2020-07-22 ----------------------------------------------
+  lifecycle::deprecate_warn(
+    "1.3.3", "gtsummary::as_flextable()", "as_flex_table()",
+    details = paste(
+      "The `as_flextable()` function graduated",
+      "from 'Experimental' status in v1.3.3. The updated function has",
+      "a new name to avoid name conflicts with {flextable}",
+      "and the ascetics of resulting tables have been improved."
+    ) %>%
+      stringr::str_wrap()
+  )
+
+  # passing args to `as_flex_table()` ------------------------------------------
+  as_flex_table(...)
 }
 
