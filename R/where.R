06085e8d7141b4455edbc53a6fe67af2be510af0
ddsjoberg <danield.sjoberg@gmail.com>
2020-08-26 15:39:47 -0400

    Update where.R

--- R/where.R
+++ R/where.R
@@ -5,12 +5,12 @@
 #' @noRd
 
 where <- function(fn) {
-  predicate <- as_function(fn)
+  predicate <- rlang::as_function(fn)
 
   function(x, ...) {
     out <- predicate(x, ...)
 
-    if (!is_bool(out)) {
+    if (!rlang::is_bool(out)) {
       abort("`where()` must be used with functions that return `TRUE` or `FALSE`.")
     }
 
