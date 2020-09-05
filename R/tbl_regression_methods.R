f59bf32fc46de02dd5ea4dbceb53ddfe8d81baed
ddsjoberg <danield.sjoberg@gmail.com>
2020-09-05 17:55:31 -0400

    removed multinom method for tbl_regression

--- R/tbl_regression_methods.R
+++ R/tbl_regression_methods.R
@@ -31,33 +31,3 @@ tbl_regression.survreg <- function(
   x, tidy_fun = function(x, ...) broom::tidy(x, ...) %>% dplyr::filter(.data$term != "Log(scale)"), ...) {
   tbl_regression.default(x = x, tidy_fun = tidy_fun, ...)
 }
-
-#' @export
-#' @rdname tbl_regression_methods
-tbl_regression.multinom <- function(x, exponentiate = FALSE, conf.level = 0.95, group_header = NULL, ...) {
-
-  inform("Constructing `tbl_regression()` objects for each outcome")
-  df_tidy <-
-    broom::tidy(x, exponentiate = exponentiate,
-                conf.level = conf.level, conf.int = TRUE) %>%
-    dplyr::nest_by(.data$y.level) %>%
-    rowwise() %>%
-    mutate(
-      # creating a tidy function that just returns the piece needed of this tidy data frame
-      tidy_data = list(function(x, ...) .data$data),
-      # running tbl_regression on each piece of the tidy tibble (for each outcome)
-      tbl_regression =
-        tbl_regression.default(x = x, tidy_fun = .data$tidy_fun, ...) %>%
-        list()
-    )
-
-  inform("Stacking `tbl_regression()` objects for each outcome using `tbl_stack()`")
-  tbl_stack(df_tidy$tbl_regression, group_header = group_header %||% df_tidy$y.level)
-}
-
-
-#' #' @export
-#' #' @rdname tbl_regression_methods
-#' tbl_regression.mipo <- function(x, tidy_fun = mice::tidy, ...) {
-#'     tbl_regression.default(x = x, tidy_fun = tidy_fun, ...)
-#' }
