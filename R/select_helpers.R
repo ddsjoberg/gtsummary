448771ca056d1a6f7a69130b23e961055bbbcdd0
ddsjoberg <danield.sjoberg@gmail.com>
2020-11-03 14:14:30 -0500

    importing all select helpers from broom.helpers now

--- R/select_helpers.R
+++ R/select_helpers.R
@@ -34,77 +34,8 @@ NULL
 
 #' @rdname select_helpers
 #' @export
-all_continuous <- function(continuous2 = TRUE) {
-  if (continuous2) con_types <- c("continuous", "continuous2")
-  else con_types <- "continuous"
-
-  .generic_selector("variable", "var_type",
-                    .data$var_type %in% con_types,
-                    fun_name = "all_continuous")
-}
-
-#' @rdname select_helpers
-#' @export
 all_continuous2 <- function() {
   .generic_selector("variable", "var_type",
                     .data$var_type %in% "continuous2",
                     fun_name = "all_continuous")
-}
-
-#' @rdname select_helpers
-#' @export
-all_dichotomous <- function() {
-  .generic_selector("variable", "var_type",
-                    .data$var_type %in% "dichotomous",
-                    fun_name = "all_dichotomous")
-}
-
-#' @rdname select_helpers
-#' @export
-all_categorical <- function(dichotomous = TRUE) {
-  types <- switch(dichotomous, c("categorical", "dichotomous")) %||% "categorical"
-
-  .generic_selector("variable", "var_type",
-                    .data$var_type %in% .env$types,
-                    fun_name = "all_categorical")
-}
-
-#' @rdname select_helpers
-#' @export
-all_interaction <- function() {
-  .generic_selector("variable", "var_type",
-                    .data$var_type %in% "interaction",
-                    fun_name = "all_interaction")
-}
-
-#' @rdname select_helpers
-#' @export
-all_intercepts <- function() {
-  .generic_selector("variable", "var_type",
-                    .data$var_type %in% "intercept",
-                    fun_name = "all_intercepts")
-}
-
-#' @rdname select_helpers
-#' @export
-all_contrasts <- function(contrasts_type = NULL) {
-  # if no types specified, select all contrasts
-  if (is.null(contrasts_type))
-    return(
-      .generic_selector("variable", "contrasts_type",
-                        !is.na(.data$contrasts_type),
-                        fun_name = "all_contrasts")
-    )
-  # otherwise, select those specified in `contrasts_type=`
-  else {
-    contrasts_type <-
-      match.arg(contrasts_type,
-                c("treatment", "sum", "poly", "helmert", "other"),
-                several.ok = TRUE)
-    return(
-      .generic_selector("variable", "contrasts_type",
-                        .data$contrasts_type %in% .env$contrasts_type,
-                        fun_name = "all_contrasts")
-    )
-  }
 }
