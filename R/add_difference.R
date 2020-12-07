e706f168b94967b80a9e4e71d7b11b03120cd01e
Daniel D. Sjoberg <danield.sjoberg@gmail.com>
2020-12-07 13:08:10 -0500

    Update add_difference.R

--- R/add_difference.R
+++ R/add_difference.R
@@ -108,7 +108,7 @@ add_difference <- function(x, test = NULL, group = NULL,
       pull(variable)
     if (!rlang::is_empty(bad_percent_vars))
       paste("{ui_code('add_difference()')} results for categorical variables",
-            "are not compatible with",
+            "may not compatible with",
             "{ui_code('tbl_summary(percent = c(\"cell\", \"row\"))')} options.",
             "Use column percentages, {ui_code('tbl_summary(percent = \"column\")')}.") %>%
       stringr::str_wrap() %>%
