a7e4847e6fb9f58ca1b15a581098dc686cb7bddd
ddsjoberg <danield.sjoberg@gmail.com>
2020-12-12 20:07:56 -0500

    Update utils-add_p.R

--- R/utils-add_p.R
+++ R/utils-add_p.R
@@ -117,7 +117,7 @@
            setequal(names(test_fun_result), c("p", "test")))
     x$df_result <-
     tibble::as_tibble(test_fun_result) %>%
-    set_names(names(test_fun_result))
+    dplyr::rename(p.value = .data$p, method = .data$test)
   else if (rlang::is_scalar_double(test_fun_result))
     x$df_result <- tibble(p.value = test_fun_result, method = NA_character_)
   else if (is.null(test_fun_result))
