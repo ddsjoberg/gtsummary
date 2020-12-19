320b20f2dffe9f379a521b87fb6750016a6c7d2a
ddsjoberg <danield.sjoberg@gmail.com>
2020-12-18 19:15:20 -0500

    documentation updates

--- R/modify_column_hide.R
+++ R/modify_column_hide.R
@@ -6,11 +6,18 @@
 #'
 #' @export
 #' @examples
+#' # Example 1 ----------------------------------
+#' # hide 95% CI, and replace with standard error
 #' modify_column_hide_ex1 <-
 #'   lm(age ~ marker + grade, trial) %>%
 #'   tbl_regression() %>%
 #'   modify_column_hide(column = ci) %>%
 #'   modify_column_unhide(column = std.error)
+#'
+#' @section Example Output:
+#' \if{html}{Example 1}
+#'
+#' \if{html}{\figure{modify_column_hide_ex1.png}{options: width=45\%}}
 modify_column_hide <- function(x, column) {
   modify_table_header(
     x = x,
