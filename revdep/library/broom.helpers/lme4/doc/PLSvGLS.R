ec821f4f785e0204af5113165f1310b0989c45f5
ddsjoberg <danield.sjoberg@gmail.com>
2021-01-10 11:58:09 -0500

    exclude revdeps from github

--- revdep/library/broom.helpers/lme4/doc/PLSvGLS.R
+++
@@ -1,9 +0,0 @@
-### R code from vignette source 'PLSvGLS.Rnw'
-
-###################################################
-### code chunk number 1: preliminaries
-###################################################
-options(width=65,digits=5)
-#library(lme4)
-
-
