ee6066beac52c48fbdf8a98f44216b615dfad8a3
ddsjoberg <danield.sjoberg@gmail.com>
2021-01-17 09:14:26 -0500

    Update test-custom_tidiers.R

--- tests/testthat/test-custom_tidiers.R
+++ tests/testthat/test-custom_tidiers.R
@@ -4,17 +4,20 @@ testthat::skip_on_cran()
 mod <- lm(age ~ marker + grade, trial)
 
 test_that("no errors/warnings with tidy_standardize", {
+  skip_if(!require("effectsize"))
   expect_error(tbl_regression(mod, tidy_fun = tidy_standardize), NA)
   expect_warning(tbl_regression(mod, tidy_fun = tidy_standardize), NA)
 })
 
 test_that("no errors/warnings with tidy_bootstrap", {
+  skip_if(!require("parameters"))
   expect_error(tbl_regression(mod, tidy_fun = tidy_bootstrap), NA)
   expect_warning(tbl_regression(mod, tidy_fun = tidy_bootstrap), NA)
 })
 
 
 test_that("no errors/warnings with pool_and_tidy_mice", {
+  skip_if(!require("mice"))
   mod_mice <-
     suppressWarnings(mice::mice(trial, m = 2)) %>%
     with(glm(response ~ age + marker + grade, family = binomial))
