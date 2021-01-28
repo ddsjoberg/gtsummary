e44054f3061b8531c3886adf587c8d963548fb50
Daniel Sjoberg <danield.sjoberg@gmail.com>
2021-01-28 09:18:35 -0500

    Update utils-misc.R

--- R/utils-misc.R
+++ R/utils-misc.R
@@ -11,8 +11,8 @@
 #' @author David Hugh-Jones
 assert_package <- function(pkg, fn, version = NULL) {
   if (is.null(version) && !requireNamespace(pkg, quietly = TRUE)) {
-    ui_oops("The {ui_value(pkg)} is required for function {ui_code(fn)}.")
-    usethis::ui_todo("Install the {ui_value(pkg)} package with the code below.")
+    ui_oops("The {ui_value(pkg)} package is required for function {ui_code(fn)}.")
+    usethis::ui_todo("Install {ui_value(pkg)} with the code below.")
     ui_code_block('install.packages("{pkg}")')
     stop("Install required package", call. = FALSE)
   }
