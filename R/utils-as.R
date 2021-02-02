4c27ac7a86fd38d964486ad35ae56285c7d48872
ddsjoberg <danield.sjoberg@gmail.com>
2021-02-02 11:11:01 -0500

    Update utils-as.R

--- R/utils-as.R
+++ R/utils-as.R
@@ -146,9 +146,12 @@
     filter(.data$column %in% .cols_to_show(x)) %>%
     rowwise() %>%
     mutate(
-      row_numbers = ifelse(is.na(.data$rows),
-                           seq_len(nrow(x$table_body)) %>% list(),
-                           .rows_expr_to_row_numbers(x$table_body, .data$rows) %>% list())
+      row_numbers = ifelse(
+        is.na(.data$rows),
+        seq_len(nrow(x$table_body)),
+        .rows_expr_to_row_numbers(x$table_body, .data$rows)
+      ) %>%
+        list()
     ) %>%
     select(-.data$rows) %>%
     unnest(.data$row_numbers) %>%
