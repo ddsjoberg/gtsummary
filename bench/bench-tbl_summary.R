c135c6aa46866670751c68ba10c94721d8a807c1
oranwutang <6965617+oranwutang@users.noreply.github.com>
2021-03-01 13:07:55 +0100

    making some tests, changes will be revrsed soon

--- bench/bench-tbl_summary.R
+++ bench/bench-tbl_summary.R
@@ -11,10 +11,10 @@ bench::mark(
   simple = tbl_summary(trial),
 
   # simple calculation with comparisons+others
-  complex = tbl_summary(trial, by = trt) %>% add_overall() %>% add_p() %>% add_q(quiet = TRUE) %>% add_n(),
+  #complex = tbl_summary(trial, by = trt) %>% add_overall() %>% add_p() %>% add_q(quiet = TRUE) %>% add_n(),
 
   # big summary
-  big_data = big_trial %>% select(age, grade, trt) %>% tbl_summary(by = trt, missing = "no") %>% add_p(),
+  #big_data = big_trial %>% select(age, grade, trt) %>% tbl_summary(by = trt, missing = "no") %>% add_p(),
 
   check = FALSE,
   min_iterations = 30
