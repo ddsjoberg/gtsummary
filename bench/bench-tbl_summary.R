2e400244a344f8e13c58993853f32315c6e7291e
oranwutang <gzapatawainberg@gmail.com>
2021-03-02 23:17:57 +0100

    new tests

--- bench/bench-tbl_summary.R
+++ bench/bench-tbl_summary.R
@@ -20,15 +20,6 @@ bench::mark(
   min_iterations = 5
 )
 
-bench::cb_fetch("https://github.com/oranwutang/gtsummary/")
-bench::cb_push("https://github.com/oranwutang/gtsummary/")
-
-
-stored_benchmarks <- bench::cb_read("https://github.com/oranwutang/gtsummary/")
-rmarkdown::render(input = here::here("bench/benchmark2html.Rmd"),
-                  output_file = here::here("bench/benchmark2html.html"),
-                  clean = T, params = list(stored_benchmarks = stored_benchmarks))
-
 # The next call was added to the benchmark yaml file, if it works, it can be deleted form this script
 # rmarkdown::render(input = here("bench/benchmark2html.Rmd"), # IMPORTANT: Relative paths doesn't work maybe solvable with {here}
 #                  output_file = here("bench/benchmark2html.html"),  # IMPORTANT: Relative paths doesn't work maybe solvable with {here}
