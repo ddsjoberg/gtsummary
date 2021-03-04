9910efb89e4deeb5c06c6b4bdb5c9b0bb0cc3377
ddsjoberg <danield.sjoberg@gmail.com>
2021-03-03 20:49:39 -0500

    Doc updates

--- R/modify_table_styling.R
+++ R/modify_table_styling.R
@@ -1,8 +1,8 @@
-#' Modify table_styling
+#' Modify Table Styling
 #'
 #' This is a function meant for advanced users to gain
 #' more control over the characteristics of the resulting
-#' gtsummary table.
+#' gtsummary table by directly modifying `.$table_styling`
 #'
 #' Review the
 #' \href{http://www.danieldsjoberg.com/gtsummary/articles/gtsummary_definition.html}{gtsummary definition}
@@ -33,6 +33,21 @@
 #' @seealso `modify_table_body()`
 #' @seealso See \href{http://www.danieldsjoberg.com/gtsummary/articles/gtsummary_definition.html}{gtsummary internals vignette}
 #' @export
+#'
+#' @section rows argument:
+#' The rows argument accepts a predicate expression that is used to specify
+#' rows to apply formatting. The expression must evaluate to a logical when
+#' evaluated in `x$table_body`. For example, to apply formatting to the age rows
+#' pass `rows = variable == "age"`. A vector of row numbers is NOT acceptable.
+#'
+#' A couple of things to note when using the `rows=` argument.
+#' 1. You can use saved objects to create the predicate argument, e.g.
+#' `rows = variable == letters[1]`.
+#' 2. The saved object cannot share a name with a column in `x$table_body`.
+#' The reason for this is that in `tbl_merge()` the columns are renamed,
+#' and the renaming process cannot disambiguate the `variable` column from
+#' an external object named `variable` in the following expression
+#' `rows = .data$variable = .env$variable`.
 
 modify_table_styling <- function(x,columns,
                                  rows = NULL,
