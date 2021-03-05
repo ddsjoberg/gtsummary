c92e1613e4009ac61e8d7b1f609de19f07125732
oranwutang <gzapatawainberg@gmail.com>
2021-03-05 21:27:05 +0100

    macos

---
+++ bench/buildReport.R
@@ -0,0 +1,5 @@
+bench::cb_fetch()
+stored_benchmarks <- bench::cb_read()
+rmarkdown::render(input = here::here("bench/benchmark2html.Rmd"),
+output_file = here::here("bench/benchmark2html.html"),
+clean = T, params = list(stored_benchmarks = stored_benchmarks))
