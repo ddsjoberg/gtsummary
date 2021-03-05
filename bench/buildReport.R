4c6cd3ade900f626ffc15c826da8e8b5909e8a3f
oranwutang <gzapatawainberg@gmail.com>
2021-03-05 21:37:09 +0100

    macos

--- bench/buildReport.R
+++ bench/buildReport.R
@@ -1,4 +1,4 @@
-bench::cb_fetch()
+# bench::cb_fetch()
 stored_benchmarks <- bench::cb_read()
 rmarkdown::render(input = here::here("bench/benchmark2html.Rmd"),
 output_file = here::here("bench/benchmark2html.html"),
