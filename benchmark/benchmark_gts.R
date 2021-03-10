38ab2c8ffe841e6514efbe9371c629dcc25327b0
oranwutang <gzapatawainberg@gmail.com>
2021-03-10 18:56:03 +0100

    Code cleaning

--- benchmark/benchmark_gts.R
+++ benchmark/benchmark_gts.R
@@ -5,16 +5,15 @@ install.packages("remotes", dependencies = c("Depends", "Imports") )
 install.packages("devtools", dependencies = c("Depends", "Imports"))
 install.packages("here", dependencies = c("Depends", "Imports"))
 
-
 library(remotes)
 library(devtools)
 library(here)
 
 # Installing different versions of the gtsummary package ----
 
+# Creating a library for each gtsummary version:
 .libPaths(new = here::here("benchmark/lib/cran"))
 .libPaths(new = here::here("benchmark/lib/github"))
-# .libPaths(new = here::here("benchmark/lib/current_branch"))
 
 # Install gtsummary-CRAN version
 install.packages(pkgs = "gtsummary", lib = here::here("benchmark/lib/cran/"), dependencies = c("Depends", "Imports"),
@@ -32,17 +31,21 @@ devtools::install(dependencies = c("Depends", "Imports"), quiet = TRUE)
 install.packages("dplyr",quiet = TRUE)
 install.packages("magrittr",quiet = TRUE)
 install.packages("ggplot2",quiet = TRUE)
-install.packages("forcats",quiet = TRUE)
+# install.packages("forcats",quiet = TRUE)
 install.packages("microbenchmark",quiet = TRUE)
 library(magrittr)
 library(dplyr)
 library(ggplot2)
-library(forcats)
+# library(forcats)
 library(microbenchmark)
 
-
 # Set how many times the benchmark will try each function:----
-bm_times <- 30
+bm_times <- 10
+
+#The functions to be tested:
+functions_list <- list(simple= "tbl_summary(trial)",
+                       complex="tbl_summary(trial, by = trt) %>% add_overall() %>% add_p() %>% add_q(quiet = TRUE) %>% add_n()",
+                       big_data = "big_trial %>% select(age, grade, trt) %>% tbl_summary(by = trt, missing = 'no') %>% add_p()")
 
 # Benchmark CRAN version ----
 # detach("package:gtsummary", unload=TRUE)
@@ -54,9 +57,7 @@ gt_ver <- as.character(packageVersion("gtsummary"))
 big_trial <- purrr::map_dfr(seq_len(10), ~trial)
 
 bm_gtsummary <- microbenchmark(
-  simple= tbl_summary(trial),
-  complex=tbl_summary(trial, by = trt) %>% add_overall() %>% add_p() %>% add_q(quiet = TRUE) %>% add_n(),
-  big_data = big_trial %>% select(age, grade, trt) %>% tbl_summary(by = trt, missing = "no") %>% add_p(),
+  list=functions_list,
   times = bm_times, unit = "s")
 
 benchmark_result <- summary(bm_gtsummary) %>% mutate("gtsummary version"=gt_ver)
@@ -68,9 +69,7 @@ library(gtsummary, lib.loc = here::here("benchmark/lib/github/"))
 gt_ver <- as.character(packageVersion("gtsummary"))
 
 bm_gtsummary <- microbenchmark(
-  simple= tbl_summary(trial),
-  complex=tbl_summary(trial, by = trt) %>% add_overall() %>% add_p() %>% add_q(quiet = TRUE) %>% add_n(),
-  big_data = big_trial %>% select(age, grade, trt) %>% tbl_summary(by = trt, missing = "no") %>% add_p(),
+  list=functions_list,
   times = bm_times, unit = "s")
 
 benchmark_result <- rbind(benchmark_result,summary(bm_gtsummary) %>% mutate("gtsummary version"=gt_ver))
@@ -84,9 +83,7 @@ gt_ver <- as.character(packageVersion("gtsummary"))
 gt_ver <- paste0(gt_ver, "current_commit")
 
 bm_gtsummary <- microbenchmark(
-  simple= tbl_summary(trial),
-  complex=tbl_summary(trial, by = trt) %>% add_overall() %>% add_p() %>% add_q(quiet = TRUE) %>% add_n(),
-  big_data = big_trial %>% select(age, grade, trt) %>% tbl_summary(by = trt, missing = "no") %>% add_p(),
+  list=functions_list,
   times = bm_times, unit = "s")
 
 benchmark_result <- rbind(benchmark_result,summary(bm_gtsummary) %>% mutate("gtsummary version"=gt_ver))
@@ -115,30 +112,16 @@ benchmark_data %>%
   ggplot(aes(color=gt_ver, x=gt_ver, y=bm_gtsummary.time/1e9))+
   facet_wrap(vars(bm_gtsummary.expr))+
   geom_jitter(alpha=0.3)+
-  # geom_hline(yintercept = median(benchmark_data$bm_gtsummary.time/1e9), linetype=2, color = "red")+
   theme_minimal()+
   theme(legend.position='none',
-                  axis.text.x = element_text(angle = 45, vjust = 0.8, hjust=1))+
+        axis.text.x = element_text(angle = 45, vjust = 0.8, hjust=1))+
   labs(y="seconds", title = "Time to run each function",
        subtitle=paste0(bm_times, " runs"),
        x="")+
   ggsave(here::here("benchmark/benchmark_jitter.png"))
 
 # Save data:----
-write.csv2(benchmark_data, file = here::here("benchmark/benchmark.csv"), row.names = FALSE, fileEncoding = "UTF-8")
+write.csv2(benchmark_data, file = here::here("benchmark/benchmark.csv"),
+           row.names = FALSE,
+           fileEncoding = "UTF-8")
 
-# rmarkdown::render(input = here::here("bench/benchmark2html.Rmd"),
-#                   output_file = here::here("bench/benchmark2html.html"),
-#                   clean = T)
-
-# benchmark_data %>% ggplot(aes(fill=gt_ver, x=bm_gtsummary.time/1e9))+
-#   geom_density(alpha=0.3)+
-#   theme_minimal()
-
-# benchmark_data %>%
-#   mutate(bm_gtsummary.time=bm_gtsummary.time/1e9) %>%
-#   select(bm_gtsummary.time, gt_ver) %>%
-#   tbl_summary(by=gt_ver) %>% add_p() %>% add_q()
-
-# relacion <-  median(benchmark_data$bm_gtsummary.time[benchmark_data$gt_ver=="1.3.0.9019_no_theme"])/
-#   median(benchmark_data$bm_gtsummary.time[benchmark_data$gt_ver=="1.3.1"])
