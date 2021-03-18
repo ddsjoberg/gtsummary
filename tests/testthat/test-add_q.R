131a3acbe131c285aa7ec614f1644ec19f280f81
ddsjoberg <danield.sjoberg@gmail.com>
2021-03-17 22:36:59 -0400

    Update test-add_q.R

--- tests/testthat/test-add_q.R
+++ tests/testthat/test-add_q.R
@@ -52,12 +52,12 @@ test_that("Checking q-values against p.adjust", {
     tbl_regression() %>%
     add_q(method = "bonferroni")
   expect_equal(
-    q_check1$table_body$q.value %>% discard(is.na),
-    q_check1$table_body$p.value %>% discard(is.na) %>% stats::p.adjust(method = "fdr")
+    q_check1$table_body$q.value %>% purrr::discard(is.na),
+    q_check1$table_body$p.value %>% purrr::discard(is.na) %>% stats::p.adjust(method = "fdr")
   )
   expect_equal(
-    q_check2$table_body$q.value %>% discard(is.na),
-    q_check2$table_body$p.value %>% discard(is.na) %>% stats::p.adjust(method = "bonferroni")
+    q_check2$table_body$q.value %>% purrr::discard(is.na),
+    q_check2$table_body$p.value %>% purrr::discard(is.na) %>% stats::p.adjust(method = "bonferroni")
   )
 })
 
