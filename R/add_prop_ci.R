d8a20c5f008e0d97850e7246830b84396b22226e
ddsjoberg <danield.sjoberg@gmail.com>
2021-09-01 18:15:41 -0400

    Update add_prop_ci.R

--- R/add_prop_ci.R
+++ R/add_prop_ci.R
@@ -3,7 +3,7 @@
 #' Add a new column with the confidence intervals for proportions.
 #'
 #' @param x A `tbl_summary` object
-#' @param pattern String indicating how the confidence interval will be placed.
+#' @param statistic String indicating how the confidence interval will be placed.
 #' Default is `"{conf.low}%, {conf.high}%"`
 #' @param method Confidence interval method. Must be one of
 #' `c("wilson", "wilson.no.correct", "exact", "asymptotic")`. Default is `"wilson"`.
@@ -20,69 +20,117 @@
 #' Methods `c("exact", "asymptotic")` are calculated with `Hmisc::binconf(method=)`.
 #'
 #' @return gtsummary table
-#' @rdname add_prop_ci
+#' @rdname add_ci
 #' @export
 #'
 #' @family tbl_summary tools
 #' @examples
 #' # Example 1 ----------------------------------
-#' add_prop_ci_ex1 <-
+#' add_ci_ex1 <-
 #'   trial %>%
 #'   select(response, trt) %>%
 #'   tbl_summary(missing = "no") %>%
-#'   add_prop_ci()
+#'   add_ci()
 #'
 #' # Example 2 ----------------------------------
-#' add_prop_ci_ex2 <-
+#' add_ci_ex2 <-
 #'   trial %>%
 #'     select(response, trt) %>%
 #'     tbl_summary(statistic = all_categorical() ~ "{p}%",
 #'                 missing = "no") %>%
-#'     add_prop_ci() %>%
+#'     add_ci() %>%
 #'     modify_cols_merge(
-#'       rows = !is.na(prop_ci_stat_0),
-#'       pattern = "{stat_0} ({prop_ci_stat_0})"
+#'       rows = !is.na(ci_stat_0),
+#'       statistic = "{stat_0} ({ci_stat_0})"
 #'     ) %>%
 #'     modify_footnote(everything() ~ NA)
 #' @section Example Output:
 #' \if{html}{Example 1}
 #'
-#' \if{html}{\figure{add_prop_ci_ex1.png}{options: width=50\%}}
+#' \if{html}{\figure{add_ci_ex1.png}{options: width=50\%}}
 #'
 #' \if{html}{Example 2}
 #'
-#' \if{html}{\figure{add_prop_ci_ex2.png}{options: width=45\%}}
-add_prop_ci <- function(x, ...) {
-  UseMethod("add_prop_ci")
+#' \if{html}{\figure{add_ci_ex2.png}{options: width=45\%}}
+add_ci <- function(x, ...) {
+  UseMethod("add_ci")
 }
 
-#' @rdname add_prop_ci
+#' @rdname add_ci
 #' @export
-add_prop_ci.tbl_summary <- function(x,
-                                    pattern = "{conf.low}%, {conf.high}%",
-                                    method = c("wilson", "wilson.no.correct",
-                                               "exact", "asymptotic"),
-                                    conf.level = 0.95,
-                                    ci_fun = NULL, ...) {
+add_ci.tbl_summary <- function(x,
+                               method = NULL,
+                               include = everything(),
+                               statistic = NULL,
+                               conf.level = 0.95,
+                               ci_fun = NULL, ...) {
   # resolving arguments --------------------------------------------------------
-  method <- match.arg(method)
-  if(!is.null(ci_fun)) ci_fun <- gts_mapper(ci_fun, "add_prop_ci(ci_fun=)")
+  include <-
+    .select_to_varnames(
+      select = {{ include }},
+      var_info = x$table_body,
+      arg_name = "include"
+    )
+  summary_type <-
+    x$meta_data %>%
+    filter(.data$variable %in% .env$include) %>%
+    select(.data$variable, .data$summary_type) %>%
+    tibble::deframe()
 
-  # checking inputs ------------------------------------------------------------
-  if (!rlang::is_string(pattern))
-    stop("`pattern=` must be a string.", call. = FALSE)
-  updated_call_list <- c(x$call_list, list(add_prop_ci = match.call()))
+  methods <- c("wilson", "wilson.no.correct",
+               "exact", "asymptotic")
+  method <-
+    .formula_list_to_named_list(
+      x = list(all_categorical() ~ "wilson", all_continuous() ~ "t.test"),
+      var_info = x$meta_data[x$meta_data$variable %in% include,],
+      arg_name = "method"
+    ) %>%
+    purrr::update_list(
+      !!!.formula_list_to_named_list(
+        x = method,
+        var_info = x$meta_data[x$meta_data$variable %in% include,],
+        arg_name = "method"
+      )
+    )
+
+  ci_fun <-
+    rep_len(list(style_sigfig), length(include)) %>%
+    rlang::set_names(include) %>%
+    purrr::update_list(
+      !!!.formula_list_to_named_list(
+        x = method,
+        var_info = x$meta_data[x$meta_data$variable %in% include,],
+        arg_name = "method"
+      )
+    )
+
+  statistic <-
+    .formula_list_to_named_list(
+      x = list(all_categorical() ~ "{conf.low}%, {conf.high}%",
+               all_continuous() ~ "{conf.low}, {conf.high}"),
+      var_info = x$meta_data[x$meta_data$variable %in% include,],
+      arg_name = "statistic"
+    ) %>%
+    purrr::update_list(
+      !!!.formula_list_to_named_list(
+        x = statistic,
+        var_info = x$meta_data[x$meta_data$variable %in% include,],
+        arg_name = "statistic"
+      )
+    )
+  updated_call_list <- c(x$call_list, list(add_ci = match.call()))
 
   # adding new column with CI --------------------------------------------------
   x <-
     x %>%
     add_stat(
-      fns = all_categorical() ~ purrr::partial(single_prop_ci,
-                                               method = method,
-                                               conf.level = conf.level,
-                                               pattern = pattern,
-                                               ci_fun = ci_fun),
-      location = list(all_dichotomous() ~ "label", all_categorical(FALSE) ~ "level")
+      fns = everything() ~ purrr::partial(single_ci,
+                                          method = method,
+                                          conf.level = conf.level,
+                                          statistic = statistic,
+                                          ci_fun = ci_fun,
+                                          summary_type = summary_type),
+      location = list(everything() ~ "label", all_categorical(FALSE) ~ "level")
     ) %>%
     # moving the CI cols to after the original stat cols (when `by=` variable present)
     # also renaming CI columns
@@ -90,7 +138,7 @@ add_prop_ci.tbl_summary <- function(x,
       function(.x) {
         cols_to_order <-
           .x %>%
-          select(all_stat_cols(), matches("^stat_\\d+_prop_ci$")) %>%
+          select(all_stat_cols(), matches("^stat_\\d+_ci$")) %>%
           names()
 
         .x %>%
@@ -98,16 +146,16 @@ add_prop_ci.tbl_summary <- function(x,
                           .before = all_of(sort(cols_to_order)[1])) %>%
           dplyr::rename_with(
             .fn = ~paste0(
-              "prop_ci_",
-              stringr::str_replace(., pattern = "_prop_ci$", replacement = "")),
-            .cols = matches("^stat_\\d+_prop_ci$")
+              "ci_",
+              stringr::str_replace(., statistic = "_ci$", replacement = "")),
+            .cols = matches("^stat_\\d+_ci$")
           )
       }
     ) %>%
     # updating CI column headers and footnotes
-    modify_header(matches("^prop_ci_stat_\\d+$") ~ paste0("**", conf.level*100, "% CI**")) %>%
+    modify_header(matches("^ci_stat_\\d+$") ~ paste0("**", conf.level*100, "% CI**")) %>%
     modify_footnote(
-      update = matches("^prop_ci_stat_\\d+$") ~ translate_text("CI = Confidence Interval"),
+      update = matches("^ci_stat_\\d+$") ~ translate_text("CI = Confidence Interval"),
       abbreviation = TRUE)
 
   # return gtsummary table -----------------------------------------------------
@@ -116,22 +164,29 @@ add_prop_ci.tbl_summary <- function(x,
 }
 
 # function to add CI for one variable
-single_prop_ci <- function(variable, by, tbl, method, conf.level, ci_fun, pattern, ...) {
-  ci_fun <-
-    ci_fun %||%
-    attr(tbl$meta_data[tbl$meta_data$variable %in% variable, ]$df_stats[[1]]$p, "fmt_fun")
-
+single_ci <- function(variable, by, tbl, method, conf.level,
+                      ci_fun, statistic, summary_type,...) {
   tbl$meta_data %>%
     filter(.data$variable %in% .env$variable) %>%
     purrr::pluck("df_stats", 1) %>%
     dplyr::rowwise() %>%
     mutate(
       ci =
-        calculate_prop_ci(x = .data$n, n = .data$N,
-                          pattern = pattern,
-                          method = method,
-                          conf.level = conf.level,
-                          ci_fun = ci_fun)
+        case_when(
+          .env$summary_type[[variable]] %in% c("categorical", "dichotomous") ~
+            calculate_prop_ci(x = .data$n, n = .data$N,
+                              statistic = statistic[[variable]],
+                              method = method[[variable]],
+                              conf.level = conf.level,
+                              ci_fun = ci_fun[[variable]]),
+          .env$summary_type[[variable]] %in% c("continuous", "continuous2") ~
+            calculate_mean_ci(data = tbl$inputs$data,
+                              statistic = statistic[[variable]],
+                              method = method[[variable]],
+                              conf.level = conf.level,
+                              ci_fun = ci_fun[[variable]])
+        )
+
     ) %>%
     select(any_of(c("col_name", "variable_levels", "ci"))) %>%
     tidyr::pivot_wider(
@@ -140,10 +195,20 @@ single_prop_ci <- function(variable, by, tbl, method, conf.level, ci_fun, patter
       names_from = .data$col_name
     ) %>%
     select(all_stat_cols()) %>%
-    dplyr::rename_with(.fn = ~paste0(., "_prop_ci"))
+    dplyr::rename_with(.fn = ~paste0(., "_ci"))
 }
 
-calculate_prop_ci <- function(x, n, pattern, method, conf.level, ci_fun) {
+calculate_mean_ci <- function(data, variable, statistic, method, conf.level, ci_fun) {
+  method_fun <-
+    switch(
+      method,
+      "t.test" = stats::t.test,
+      "poisson.test" = stats::poisson.test
+    )
+
+}
+
+calculate_prop_ci <- function(x, n, statistic, method, conf.level, ci_fun) {
   # calculate CI
   if (method %in% c("wilson", "wilson.no.correct")) {
     df_ci <-
@@ -153,7 +218,7 @@ calculate_prop_ci <- function(x, n, pattern, method, conf.level, ci_fun) {
       broom::tidy()
   }
   else if (method %in% c("exact", "asymptotic")) {
-    assert_package("Hmisc", fn = 'add_prop_ci(method = c("exact", "asymptotic"))')
+    assert_package("Hmisc", fn = 'add_ci(method = c("exact", "asymptotic"))')
     df_ci <-
       Hmisc::binconf(x = x, n = n,
                      method = method, alpha = 1 - conf.level) %>%
@@ -165,7 +230,7 @@ calculate_prop_ci <- function(x, n, pattern, method, conf.level, ci_fun) {
   df_ci %>%
     select(all_of(c("conf.low", "conf.high"))) %>%
     dplyr::mutate_all(ci_fun) %>%
-    glue::glue_data(pattern) %>%
+    glue::glue_data(statistic) %>%
     as.character()
 }
 
