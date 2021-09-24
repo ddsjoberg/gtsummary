c7187ffeb00090d312850d10219cc29712c59ebe
ddsjoberg <danield.sjoberg@gmail.com>
2021-09-24 12:16:58 -0400

    Update test-tbl_custom_summary.R

--- tests/testthat/test-tbl_custom_summary.R
+++ tests/testthat/test-tbl_custom_summary.R
@@ -15,8 +15,24 @@ test_that("tbl_custom_summary() basics", {
       ) %>%
       add_overall(last = TRUE) %>%
       modify_footnote(all_stat_cols() ~ "Mean age") %>%
+      modify_column_unhide(everything()) %>%
       as_tibble(col_labels = FALSE),
     NA
+  )
+
+  expect_equal(
+    tbl1 %>%
+      filter(variable == "grade", row_type == "level") %>%
+      select(all_stat_cols(F)),
+    trial %>%
+      select(age, grade, trt) %>%
+      filter(complete.cases(.)) %>%
+      group_by(trt, grade) %>%
+      dplyr::summarise(mean_age = mean(age), .groups = "drop") %>%
+      tidyr::pivot_wider(id_cols = grade, names_from = trt, values_from = mean_age) %>%
+      select(-1) %>%
+      set_names(c("stat_1", "stat_2")) %>%
+      dplyr::mutate_all(~style_number(., 1))
   )
 
 })
