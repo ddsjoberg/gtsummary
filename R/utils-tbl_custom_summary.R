4e3ba33c47d112fef3a29d0c2c34072f3984f5a2
Joseph <joseph@larmarange.net>
2021-09-29 12:44:37 +0200

    ratio_summary

--- R/utils-tbl_custom_summary.R
+++ R/utils-tbl_custom_summary.R
@@ -1,7 +1,7 @@
 #' Summarize a continuous variable
 #'
 #' \lifecycle{experimental}
-#' This helper to be used with [tbl_custom_summary()] creates a function
+#' This helper, to be used with [tbl_custom_summary()], creates a function
 #' summarizing a continuous variable.
 #'
 #' @param variable String indicating the name of the variable to be summarized. This
@@ -46,5 +46,79 @@ continuous_summary <- function(variable) {
       summary_type = "continuous"
     ) %>%
       dplyr::select(-.data$variable, -.data$stat_display)
+  }
+}
+
+
+#' Summarize the ratio of two variables
+#'
+#' \lifecycle{experimental}
+#' This helper, to be used with [tbl_custom_summary()], creates a function
+#' computing the ratio of two continuous variables and its confidence interval.
+#'
+#' @param numerator String indicating the name of the variable to be summed
+#' for computing the numerator.
+#' @param denominator String indicating the name of the variable to be summed
+#' for computing the denominator.
+#' @param na.rm Should missing values be removed before summing the numerator
+#' and the denominator? (default is `TRUE`)
+#' @param conf.level Confidence level for the returned confidence interval.
+#' Must be strictly greater than 0 and les than 1. Default to 0.95, which
+#' corresponds to a 95 percent confidence interval.
+#'
+#' @details
+#' Computed statistics:
+#' \itemize{
+#'   \item `{num}` sum of the variable defined by `numerator`
+#'   \item `{denom}` sum of the variable defined by `denominator`
+#'   \item `{ratio}` ratio of `num` by `denom`
+#'   \item `{conf.low}` lower confidence interval
+#'   \item `{conf.high}` upper confidence interval
+#' }
+#'
+#' Condidence interval is computed with [stats::poisson.test()], if and only if
+#' `num` is an integer.
+#'
+#' @export
+#' @family tbl_custom_summary tools
+#' @author Joseph Larmarange
+#' @examples
+#' # Example 1 ----------------------------------
+#' ratio_summary_ex1 <-
+#'   trial %>%
+#'   tbl_custom_summary(
+#'     include = c("stage", "grade"),
+#'     by = "trt",
+#'     stat_fns = everything() ~ ratio_summary("response", "ttdeath"),
+#'     statistic = everything() ~ "{ratio} [{conf.low}; {conf.high}] ({num}/{denom})",
+#'     digits = everything() ~ c(3, 2, 2, 0, 0),
+#'     overall_row = TRUE,
+#'     overall_row_label = "All stages & grades"
+#'   ) %>%
+#'   bold_labels() %>%
+#'   modify_footnote(
+#'     update = all_stat_cols() ~ "Ratio [95% CI] (n/N)"
+#'   )
+#' @section Example Output:
+#' \if{html}{Example 1}
+#'
+#' \if{html}{\figure{ratio_summary_ex1.png}{options: width=31\%}}
+ratio_summary <- function(numerator, denominator, na.rm = TRUE, conf.level = 0.95) {
+  function(data, ...) {
+    num <- sum(data[[numerator]], na.rm = na.rm)
+    denom <- sum(data[[denominator]], na.rm = na.rm)
+    ratio <- num / denom
+    if (num %% 1 == 0) {
+      ci_poisson <- stats::poisson.test(num, denom, conf.level = conf.level)$conf.int
+    } else {
+      ci_poisson <- c(NA, NA)
+    }
+    dplyr::tibble(
+      num = num,
+      denom = denom,
+      ratio = ratio,
+      conf.low = ci_poisson[1],
+      conf.high = ci_poisson[2]
+    )
   }
 }
