c0177c963905b7111f775d98dbb78f25b0fa2728
Jessica Lavery <46568753+jalavery@users.noreply.github.com>
2021-10-03 11:08:50 -0400

    Add tbl_split() (#994)
    * add tbl_split from bstfun
    
    * add test, update doc
    
    * misc updates
    
    * increment version number
    
    * Update _pkgdown.yml
    
    * Update _pkgdown.yml
    
    Co-authored-by: Sjoberg <SjobergD@mskcc.org>
    Co-authored-by: Daniel Sjoberg <danield.sjoberg@gmail.com>

---
+++ tests/testthat/test-tbl_split.R
@@ -0,0 +1,8 @@
+t1 <- tbl_summary(trial)
+
+test_that("no errors/warnings with standard use", {
+  expect_error(t1, NA)
+  expect_warning(t1, NA)
+  expect_error(tbl_split(t1, variables = age), NA)
+  expect_warning(tbl_split(t1, variables = age), NA)
+})
