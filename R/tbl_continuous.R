758907a13515b8c75820bf4ef69c262282aba0db
Sjoberg <SjobergD@mskcc.org>
2021-10-11 15:03:26 -0400

    updates

--- R/tbl_continuous.R
+++ R/tbl_continuous.R
@@ -3,14 +3,13 @@
 #' Summarize a continuous variable by one or more categorical variables
 #'
 #' @param variable Variable name of the continuous column to be summarized
-#' @param include Vector of categorical variable names
-#' @param by Optional name of single categorical variable. Default it `NULL`
-#' @param statistic String specifying the continuous summary statistics to
-#' display.  The default is `"{median} ({p25}, {p75})"`. See
-#' `tbl_summary(statistic=)` argument for details.
-#' @param digits An integer specifying the number of decimal places to round
-#' the variable to. Default is `NULL` and the number of decimal places is guessed
-#' based on the distribution of the continuous variable.
+#' @param digits List of formulas specifying the number of decimal places
+#' to round continuous summary statistics. If not specified, an appropriate
+#' number of decimals to round statistics will be guessed based on the
+#' the variable's distribution.
+#' @param statistic List of formulas specifying types of summary statistics
+#' to display for each variable. The default is
+#' `everything() ~ {median} ({p25}, {p75})`
 #' @inheritParams tbl_summary
 #'
 #' @return a gtsummary table
@@ -44,10 +43,10 @@
 
 tbl_continuous <- function(data,
                            variable,
-                           include,
+                           include = everything(),
                            digits = NULL,
                            by = NULL,
-                           statistic = everything() ~ "{median} ({p25}, {p75})",
+                           statistic = NULL,
                            label = NULL) {
   # evaluate inputs ------------------------------------------------------------
   variable <- .select_to_varnames(data = data, select = {{ variable }},
@@ -57,18 +56,43 @@ tbl_continuous <- function(data,
   by <- .select_to_varnames(data = data, select = {{ by }},
                             select_single = TRUE, arg_name = "by")
   digits <-
-    digits %||% continuous_digits_guess(data, variable, "continuous")
+    rep_len(
+      list(continuous_digits_guess(data, variable, "continuous")),
+      length.out = length(include)
+    ) %>%
+    rlang::set_names(include) %>%
+    purrr::list_modify(
+      !!!.formula_list_to_named_list(digits, data = data, arg_name = "digits")
+    )
+  statistic <-
+    rep_len(list("{median} ({p25}, {p75})"), length.out = length(include)) %>%
+    rlang::set_names(include) %>%
+    purrr::list_modify(
+      !!!.formula_list_to_named_list(statistic, data = data, arg_name = "statistic")
+    )
+
+  label <-
+    .formula_list_to_named_list(label, data = data, arg_name = "label")
+
+  # saving function inputs
+  tbl_continuous_inputs <- as.list(environment())
 
   # calculate tbl_continuous tables --------------------------------------------
-  tbl_custom_summary(
-    data, data,
-    by = by,
-    stat_fns = everything() ~ continuous_summary(variable),
-    statistic = statistic,
-    digits = everything() ~ digits,
-    label = label,
-    include = include,
-    type = all_of(include %>% setdiff(c(variable, by))) ~ "categorical"
-  )
+  result <-
+    tbl_custom_summary(
+      data, data,
+      by = by,
+      stat_fns = everything() ~ continuous_summary(variable),
+      statistic = statistic,
+      digits = digits,
+      label = label,
+      include = include,
+      type = all_of(include %>% setdiff(c(variable, by))) ~ "categorical"
+    )
+  result[["inputs"]] <- tbl_continuous_inputs
+  result[["call_list"]] <- list(tbl_continuous = match.call())
+
+  # return result --------------------------------------------------------------
+  result
 }
 
