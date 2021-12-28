aa8006e29167fbd4c97054f478fdb74e77288b2a
Daniel Sjoberg <danield.sjoberg@gmail.com>
2021-12-28 08:45:32 -0500

    Update test-add_p.tbl_continuous.R

--- tests/testthat/test-add_p.tbl_continuous.R
+++ tests/testthat/test-add_p.tbl_continuous.R
@@ -45,4 +45,26 @@ test_that("add_p.tbl_continuous() works", {
       add_p(everything() ~ "lme4", group = "stage"),
     NA
   )
+
+
+
+
+  expect_message(
+    tbl_continuous(
+      data = trial,
+      variable = age,
+      include = trt,
+      by = grade
+    ) %>%
+      add_p(everything() ~ "wilcox.test")
+  )
+  expect_error(
+    tbl_continuous(
+      data = trial,
+      variable = age,
+      include = trt,
+      by = grade
+    ) %>%
+      add_p(everything() ~ "wilcox.test.NOT.A.TEST")
+  )
 })
