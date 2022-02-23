8167da93a5e283c85ebea77483b7aeda5c4b9923
Daniel Sjoberg <danield.sjoberg@gmail.com>
2022-02-22 19:56:06 -0500

    updated round function to account for the default round-to-even in base round()

---
+++ tests/testthat/test-style_number.R
@@ -0,0 +1,17 @@
+test_that("style_number() works", {
+
+  expect_equal(
+    style_number(c(-0.5, 0.5)),
+    c("-1", "1")
+  )
+
+  expect_equal(
+    style_number(c(-1.25, 1.25), digits = 1),
+    c("-1.3", "1.3")
+  )
+
+  expect_equal(
+    style_number(c(-1.25, 1.255, 0.499999999), digits = c(1, 2, 0)),
+    c("-1.3", "1.26", "0")
+  )
+})
