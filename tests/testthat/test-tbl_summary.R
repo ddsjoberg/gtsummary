f3e9cd2aa90b38238ea2643249485b7e234c1f14
Daniel Sjoberg <danield.sjoberg@gmail.com>
2022-03-04 20:48:07 -0500

    Update test-tbl_summary.R

--- tests/testthat/test-tbl_summary.R
+++ tests/testthat/test-tbl_summary.R
@@ -663,10 +663,12 @@ test_that("no error with factor variable with all NA and no specifed levels", {
 
 
 test_that("modify_*() family works", {
-  tbl1 <-
+  tbl0 <-
     trial %>%
     select(trt, age, grade) %>%
-    tbl_summary(by = trt) %>%
+    tbl_summary(by = trt)
+  tbl1 <-
+    tbl0 %>%
     add_difference() %>%
     add_n() %>%
     add_overall() %>%
@@ -676,9 +678,7 @@ test_that("modify_*() family works", {
     modify_footnote(label = "N = {N}") %>%
     modify_caption("N = {N}")
   tbl2 <-
-    trial %>%
-    select(trt, age, grade) %>%
-    tbl_summary(by = trt) %>%
+    tbl0 %>%
     add_overall() %>%
     add_p() %>%
     add_n() %>%
@@ -724,5 +724,12 @@ test_that("modify_*() family works", {
     tbl2$table_styling$caption,
     "N = 200",
     ignore_attr = TRUE
+  )
+
+  expect_equal(
+    tbl_merge(list(tbl0, tbl0))$table_styling$header %>%
+      filter(startsWith(column, "stat_")) %>%
+      dplyr::pull(modify_stat_n),
+    c(98, 102, 98, 102)
   )
 })
