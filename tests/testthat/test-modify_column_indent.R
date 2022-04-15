99f82a75e903b789059ed24fe2c8dc42b53a5789
Daniel Sjoberg <danield.sjoberg@gmail.com>
2022-04-15 18:08:13 -0400

    Added modify_column_indent function (#1198)
    * Added modify_column_indent function
    
    * updates
    
    * Update test-modify_column_indent.R

---
+++ tests/testthat/test-modify_column_indent.R
@@ -0,0 +1,15 @@
+test_that("modify_column_indent() works", {
+  expect_error(
+    tbl <-
+      trial %>%
+      select(grade) %>%
+      tbl_summary() %>%
+      modify_column_indent(columns = label, undo = TRUE),
+    NA
+  )
+
+  expect_equal(
+    tbl$table_styling$text_format$undo_text_format,
+    c(FALSE, TRUE)
+  )
+})
