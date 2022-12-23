6430d45308f73e05a26c6d772408f1c9f4743a9d
Daniel Sjoberg <danield.sjoberg@gmail.com>
2022-12-22 21:51:11 -0500

    Update as_flex_table.R

--- R/as_flex_table.R
+++ R/as_flex_table.R
@@ -347,7 +347,7 @@ table_styling_to_flextable_calls <- function(x, ...) {
   if (!is.null(x$table_styling$source_note)) {
     flextable_calls[["source_note"]] <-
       expr(
-        flextable::footnote(value = flextable::as_paragraph(!!x$table_styling$source_note), ref_symbols = "")
+        flextable::footnote(value = flextable::as_paragraph(!!x$table_styling$source_note), ref_symbols = " ")
       )
   }
 
