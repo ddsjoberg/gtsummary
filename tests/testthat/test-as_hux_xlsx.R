15770daeec13ddd7d616838b53bf8e20059f0bf1
Daniel Sjoberg <danield.sjoberg@gmail.com>
2023-01-02 08:54:13 -0500

    As snaps (#1431)
    * snaps
    
    * updates
    
    * snap updates

--- tests/testthat/test-as_hux_xlsx.R
+++ tests/testthat/test-as_hux_xlsx.R
@@ -5,11 +5,11 @@ test_that("as_hux_xlsx() works", {
     add_p(source_note = TRUE)
 
   expect_error(
-    as_hux_xlsx(tbl, file = file.path(tempdir(), "testing.xlsx"), bold_header_rows = TRUE),
+    hux <- as_hux_xlsx(tbl, file = file.path(tempdir(), "testing.xlsx"), bold_header_rows = TRUE),
     NA
   )
   expect_error(
-    as_hux_xlsx(tbl, file = file.path(tempdir(), "testing.xlsx"), bold_header_rows = FALSE),
+    hux <- as_hux_xlsx(tbl, file = file.path(tempdir(), "testing.xlsx"), bold_header_rows = FALSE),
     NA
   )
 })
