52eb4215f78d9e32d88932a157662e95560c44be
Daniel Sjoberg <danield.sjoberg@gmail.com>
2023-10-08 08:01:25 -0700

    Update test-vetted_models-clogit.R

--- tests/testthat/test-vetted_models-clogit.R
+++ tests/testthat/test-vetted_models-clogit.R
@@ -25,7 +25,6 @@ skip_on_cran()
 skip_if(!isTRUE(as.logical(Sys.getenv("CI"))))
 skip_if_not(broom.helpers::.assert_package("car", pkg_search = "gtsummary", boolean = TRUE))
 skip_if_not(broom.helpers::.assert_package("survival", pkg_search = "gtsummary", boolean = TRUE))
-library(dplyr)
 library(survival)
 
 # clogit() ---------------------------------------------------------------------
