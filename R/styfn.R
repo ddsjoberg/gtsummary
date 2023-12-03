3ec4dcfd7762e9225ba2b7b6671c6e59b0303c79
Daniel Sjoberg <danield.sjoberg@gmail.com>
2023-12-02 20:33:49 -0800

    in progress

---
+++ R/styfn.R
@@ -0,0 +1,47 @@
+#' Style Functions
+#'
+#' Similar to the `style_*()` family of functions, but these functions return
+#' a `style_*()` **function** rather than performing the styling.
+#'
+#' @param digits,big.mark,decimal.mark,scale,prepend_p,symbol,... arguments
+#' passed to the `style_*()` functions
+#'
+#' @return a function
+#' @name styfn
+#' @family style tools
+#'
+#' @examples
+#' my_style <- styfn_number(digits = 1)
+#' my_style(3.14)
+NULL
+
+#' @rdname styfn
+#' @export
+styfn_number <- function(digits = 0, big.mark = NULL, decimal.mark = NULL, scale = 1, ...) {
+  function(x) style_number(x, digits = digits, big.mark = big.mark, decimal.mark = decimal.mark, scale = scale, ...)
+}
+
+#' @rdname styfn
+#' @export
+styfn_sigfig <- function(digits = 2, scale = 1, big.mark = NULL, decimal.mark = NULL, ...) {
+  function(x) style_sigfig(x, digits = digits, scale = scale, big.mark = big.mark, decimal.mark = decimal.mark, ...)
+}
+
+#' @rdname styfn
+#' @export
+styfn_pvalue <- function(x, digits = 1, prepend_p = FALSE, big.mark = NULL, decimal.mark = NULL, ...) {
+  function(x) styfn_pvalue(x, digits = digits, prepend_p = prepend_p, big.mark = big.mark, decimal.mark = decimal.mark, ...)
+}
+
+#' @rdname styfn
+#' @export
+styfn_ratio <- function(digits = 2, big.mark = NULL, decimal.mark = NULL, ...) {
+  function(x) style_ratio(x, digits = digits, big.mark = big.mark, decimal.mark = decimal.mark, ...)
+}
+
+#' @rdname styfn
+#' @export
+styfn_percent <- function(symbol = FALSE, digits = 0, big.mark = NULL, decimal.mark = NULL, ...) {
+  function(x) style_percent(x, symbol = symbol, digits = digits, big.mark = big.mark, decimal.mark = decimal.mark, ...)
+}
+
---
+++ R/style_percent.R
@@ -0,0 +1,34 @@
+#' Style percentages
+#'
+#' @param x numeric vector of percentages
+#' @param digits number of digits to round large percentages (i.e. greater than 10%).
+#' Smaller percentages are rounded to `digits + 1` places.
+#' Default is `0`
+#' @param symbol Logical indicator to include percent symbol in output.
+#' Default is `FALSE`.
+#' @inheritParams style_number
+#' @export
+#' @return A character vector of styled percentages
+#' @family style tools
+#' @seealso See Table Gallery \href{https://www.danieldsjoberg.com/gtsummary/articles/gallery.html}{vignette} for example
+#' @author Daniel D. Sjoberg
+#' @examples
+#' percent_vals <- c(-1, 0, 0.0001, 0.005, 0.01, 0.10, 0.45356, 0.99, 1.45)
+#' style_percent(percent_vals)
+#' style_percent(percent_vals, symbol = TRUE, digits = 1)
+style_percent <- function(x, symbol = FALSE, digits = 0, big.mark = NULL, decimal.mark = NULL, ...) {
+  y <- dplyr::case_when(
+    x * 100 >= 10 ~ style_number(x * 100, digits = digits, big.mark = big.mark, decimal.mark = decimal.mark, ...),
+    x * 100 >= 10^(-(digits + 1)) ~ style_number(x * 100, digits = digits + 1, big.mark = big.mark, decimal.mark = decimal.mark, ...),
+    x > 0 ~ paste0("<", style_number(
+      x = 10^(-(digits + 1)), digits = digits + 1, big.mark = big.mark,
+      decimal.mark = decimal.mark, ...
+    )),
+    x == 0 ~ "0"
+  )
+
+  # adding percent symbol if requested
+  if (symbol == TRUE) y <- ifelse(!is.na(y), paste0(y, "%"), y)
+  attributes(y) <- attributes(unclass(x))
+  return(y)
+}
