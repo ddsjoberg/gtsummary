0c213cb7f694b286581e3c098ec4d0d56047ee42
Daniel Sjoberg <danield.sjoberg@gmail.com>
2024-05-22 15:21:32 -0700

    progress

---
+++ tests/testthat/_snaps/remove_row_type.md
@@ -0,0 +1,8 @@
+# remove_row_type(type) messaging
+
+    Code
+      remove_row_type(tbl_summary(trial, include = c(response, age, grade)), type = "reference")
+    Condition
+      Error in `remove_row_type()`:
+      ! Column(s) "reference_row" are not present in `x$table_body`, and function cannot be used to remove these rows.
+
---
+++ tests/testthat/test-remove_row_type.R
@@ -0,0 +1,47 @@
+test_that("remove_row_type(type) works", {
+  tbl1 <- tbl_summary(trial, include = c(response, age, grade))
+  tbl2 <- lm(age ~ grade + response, trial) |> tbl_regression()
+
+  expect_equal(
+    remove_row_type(tbl1, type = "header", variables = c("grade", "age")) |>
+      as.data.frame(col_label = FALSE) |>
+      dplyr::pull("label"),
+    c("Tumor Response", "Unknown", "Age", "Unknown", "I", "II", "III")
+  )
+
+  expect_equal(
+    remove_row_type(tbl1, type = "missing", variables = c("grade", "age")) |>
+      as.data.frame(col_label = FALSE) |>
+      dplyr::pull("label"),
+    c("Tumor Response", "Unknown", "Age", "Grade", "I", "II", "III")
+  )
+
+  expect_equal(
+    remove_row_type(tbl1, type = "level", variables = c("grade", "age"), level_value = "III") |>
+      as.data.frame(col_label = FALSE) |>
+      dplyr::pull("label"),
+    c("Tumor Response", "Unknown", "Age", "Unknown", "Grade", "I", "II")
+  )
+
+  expect_equal(
+    remove_row_type(tbl1, type = "all", variables = c("grade", "age")) |>
+      as.data.frame(col_label = FALSE) |>
+      dplyr::pull("label"),
+    c("Tumor Response", "Unknown")
+  )
+
+  expect_equal(
+    remove_row_type(tbl2, type = "reference", variables = c("grade", "response")) |>
+      as.data.frame(col_label = FALSE) |>
+      dplyr::pull("label"),
+    c("Grade", "II", "III", "Tumor Response")
+  )
+})
+
+test_that("remove_row_type(type) messaging", {
+  expect_snapshot(
+    error = TRUE,
+    tbl_summary(trial, include = c(response, age, grade)) |>
+      remove_row_type(type = "reference")
+  )
+})
