30cab2209c6201bc20e4f3bc636963c3407cff70
Daniel Sjoberg <danield.sjoberg@gmail.com>
2024-05-22 18:56:41 -0700

    v2.0 `style_*()` migration (#1688)
    * in progress
    
    * style updates
    
    * progress

---
+++ tests/testthat/test-style_percent.R
@@ -0,0 +1,20 @@
+test_that("style_percent() works", {
+  percent_vals <- c(-1, 0, 0.00001, 0.0001, 0.005, 0.01, 0.10, 0.45356, 0.99, 1.45)
+
+  expect_equal(
+    style_percent(percent_vals, symbol = TRUE),
+    c(NA, "0%", "<0.1%", "<0.1%", "0.5%", "1.0%", "10%", "45%", "99%", "145%")
+  )
+
+  expect_equal(
+    style_percent(percent_vals, symbol = TRUE, digits = 1),
+    c(NA, "0%", "<0.01%", "0.01%", "0.50%", "1.00%", "10.0%", "45.4%", "99.0%", "145.0%")
+  )
+
+  vec2 <- c(one = 0.99, two = 0.0005)
+  expect_equal(
+    attributes(vec2),
+    style_percent(vec2) |> attributes()
+  )
+})
+
---
+++ tests/testthat/test-style_sigfig.R
@@ -0,0 +1,25 @@
+test_that("style_sigfig() works", {
+  expect_equal(
+    c(1.0001, 1, 0.9999, 0.90000) |> style_sigfig(digits = 1),
+    c("1", "1", "1", "0.9")
+  )
+
+  expect_true(
+    style_sigfig(-9.98) == "-10"
+  )
+
+  expect_equal(
+    style_sigfig(seq(from = 9.94, to = 10, by = 0.01)),
+    c("9.9", "10", "10", "10", "10", "10", "10")
+  )
+
+  expect_true(
+    style_sigfig(0.97, digits = 1) == "1"
+  )
+
+  vec2 <- c(one = 0.99, two = 0.0005)
+  expect_equal(
+    attributes(vec2),
+    style_sigfig(vec2) %>% attributes()
+  )
+})
