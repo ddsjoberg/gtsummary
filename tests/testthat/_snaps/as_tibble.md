e0c82f9b34ba308a47e71f6aa7affd647c3855e4
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2024-06-07 11:48:44 -0400

    A bit of clean up

--- tests/testthat/_snaps/as_tibble.md
+++ tests/testthat/_snaps/as_tibble.md
@@ -3,27 +3,13 @@
     Code
       as.data.frame(res)
     Output
-             **Characteristic**       **N = 200**
-      1  Chemotherapy Treatment              <NA>
-      2                  Drug A          98 (49%)
-      3                  Drug B         102 (51%)
-      4                     Age       47 (38, 57)
-      5                 Unknown                11
-      6    Marker Level (ng/mL) 0.64 (0.22, 1.41)
-      7                 Unknown                10
-      8                 T Stage              <NA>
-      9                      T1          53 (27%)
-      10                     T2          54 (27%)
-      11                     T3          43 (22%)
-      12                     T4          50 (25%)
-      13                  Grade              <NA>
-      14                      I          68 (34%)
-      15                     II          68 (34%)
-      16                    III          64 (32%)
-      17         Tumor Response          61 (32%)
-      18                Unknown                 7
-      19           Patient Died         112 (56%)
-      20 Months to Death/Censor 22.4 (15.9, 24.0)
+            **Characteristic** **N = 200**
+      1 Chemotherapy Treatment        <NA>
+      2                 Drug A    98 (49%)
+      3                 Drug B   102 (51%)
+      4                    Age 47 (38, 57)
+      5                Unknown          11
+      6           Patient Died   112 (56%)
 
 ---
 
@@ -54,27 +40,13 @@
     Code
       as.data.frame(res)
     Output
-                 **Characteristic**       **N = 200**
-      1  __Chemotherapy Treatment__              <NA>
-      2                    _Drug A_          98 (49%)
-      3                    _Drug B_         102 (51%)
-      4                     __Age__       47 (38, 57)
-      5                   _Unknown_                11
-      6    __Marker Level (ng/mL)__ 0.64 (0.22, 1.41)
-      7                   _Unknown_                10
-      8                 __T Stage__              <NA>
-      9                        _T1_          53 (27%)
-      10                       _T2_          54 (27%)
-      11                       _T3_          43 (22%)
-      12                       _T4_          50 (25%)
-      13                  __Grade__              <NA>
-      14                        _I_          68 (34%)
-      15                       _II_          68 (34%)
-      16                      _III_          64 (32%)
-      17         __Tumor Response__          61 (32%)
-      18                  _Unknown_                 7
-      19           __Patient Died__         112 (56%)
-      20 __Months to Death/Censor__ 22.4 (15.9, 24.0)
+                **Characteristic** **N = 200**
+      1 __Chemotherapy Treatment__        <NA>
+      2                   _Drug A_    98 (49%)
+      3                   _Drug B_   102 (51%)
+      4                    __Age__ 47 (38, 57)
+      5                  _Unknown_          11
+      6           __Patient Died__   112 (56%)
 
 ---
 
@@ -106,11 +78,11 @@
       as.data.frame(res)
     Output
         **Characteristic** **OR** **95% CI** **p-value**
-      1            __Age__ 1,0191 1.00, 1.04    __0.10__
-      2          __Grade__   <NA>       <NA>        <NA>
-      3                _I_   <NA>       <NA>        <NA>
-      4               _II_ 0,8535 0.39, 1.85       0.688
-      5              _III_ 1,0136 0.47, 2.16       0.972
+      1                Age 1,0191 1.00, 1.04        0.10
+      2              Grade   <NA>       <NA>        <NA>
+      3                  I   <NA>       <NA>        <NA>
+      4                 II 0,8535 0.39, 1.85       0.688
+      5                III 1,0136 0.47, 2.16       0.972
 
 ---
 
@@ -118,13 +90,13 @@
       as.data.frame(res)
     Output
         **Characteristic**    **N** **OR**  **95% CI** **p-value**
-      1              _Age_   183.00   1.02 0.997, 1.04        0.10
-      2            _Grade_ 193.0000   <NA>        <NA>        <NA>
-      3              __I__     <NA>   <NA>        <NA>        <NA>
-      4             __II__     <NA>   0.95 0.446, 2.00         0.9
-      5            __III__     <NA>   1.10 0.524, 2.29         0.8
+      1                Age   183.00   1.02 0.997, 1.04        0.10
+      2              Grade 193.0000   <NA>        <NA>        <NA>
+      3                  I     <NA>   <NA>        <NA>        <NA>
+      4                 II     <NA>   0.95 0.446, 2.00         0.9
+      5                III     <NA>   1.10 0.524, 2.29         0.8
 
-# as_tibble works with column merging
+# as_tibble works with tbl_merge
 
     Code
       as.data.frame(res)
@@ -151,7 +123,7 @@
       3           6       -6.9  -10, -3.7    <0.001          —          —      <NA>
       4           8        -12  -14, -8.9    <0.001       -4.6 -6.8, -2.4    <0.001
 
-# as_tibble works with column grouping
+# as_tibble works with grouped columns
 
     Code
       as.data.frame(res)
--- tests/testthat/test-as_tibble.R
+++ tests/testthat/test-as_tibble.R
@@ -1,10 +1,11 @@
-t1 <- trial |>
+t1_summary <- trial |>
+  select(trt, age, death) |>
   tbl_summary()
 
-t2 <- glm(response ~ age + grade, trial, family = binomial(link = "logit")) |>
+t2_regression <- glm(response ~ age + grade, trial, family = binomial(link = "logit")) |>
   tbl_regression(exponentiate = TRUE)
 
-t3 <- tbl_uvregression(
+t3_uvregression <- tbl_uvregression(
   trial |> dplyr::select(response, age, grade),
   method = glm,
   y = response,
@@ -12,60 +13,65 @@ t3 <- tbl_uvregression(
   exponentiate = TRUE
 )
 
+t4_regression_merged <- tbl_merge(list(
+  lm(mpg ~ factor(cyl), mtcars) |> tbl_regression(),
+  lm(mpg ~ factor(cyl), mtcars |> dplyr::filter(cyl != 4)) |> tbl_regression()
+))
+
 test_that("as_tibble works with standard use", {
-  expect_silent(res <- as_tibble(t1))
-  expect_silent(as_tibble(t1, return_calls = TRUE))
+  expect_silent(res <- as_tibble(t1_summary))
+  expect_silent(as_tibble(t1_summary, return_calls = TRUE))
   expect_snapshot(res |> as.data.frame())
 
-  expect_silent(res <- as_tibble(t2))
+  expect_silent(res <- as_tibble(t2_regression))
   expect_snapshot(res |> as.data.frame())
 
-  expect_silent(res <- as_tibble(t3))
+  expect_silent(res <- as_tibble(t3_uvregression))
   expect_snapshot(res |> as.data.frame())
 })
 
 test_that("as_tibble(col_labels=) works", {
   expect_equal(
-    as_tibble(t2, col_labels = FALSE) |> names(),
+    as_tibble(t2_regression, col_labels = FALSE) |> names(),
     c("label", "estimate", "conf.low", "p.value")
   )
 
   expect_equal(
-    as_tibble(t2) |> names(),
+    as_tibble(t2_regression) |> names(),
     c("**Characteristic**", "**OR**", "**95% CI**", "**p-value**")
   )
 })
 
-t1 <- t1 |>
-  bold_labels() |>
-  italicize_levels()
-
-t2 <- t2 |>
-  bold_labels() |>
-  italicize_levels()
-
-t3 <- t3 |>
-  bold_levels() |>
-  italicize_labels()
-
 test_that("as_tibble works with bold/italics", {
-  expect_silent(res <- as_tibble(t1))
+  t1_summary <- t1_summary |>
+    bold_labels() |>
+    italicize_levels()
+
+  t2_regression <- t2_regression |>
+    bold_labels() |>
+    italicize_levels()
+
+  t3_uvregression <- t3_uvregression |>
+    bold_levels() |>
+    italicize_labels()
+
+  expect_silent(res <- as_tibble(t1_summary))
   expect_snapshot(res |> as.data.frame())
 
-  expect_silent(res <- as_tibble(t2))
+  expect_silent(res <- as_tibble(t2_regression))
   expect_snapshot(res |> as.data.frame())
   expect_equal(
-    as_tibble(t2, col_labels = FALSE)$label,
+    as_tibble(t2_regression, col_labels = FALSE)$label,
     c("__Age__", "__Grade__", "_I_", "_II_", "_III_")
   )
 
-  expect_silent(res <- as_tibble(t3))
+  expect_silent(res <- as_tibble(t3_uvregression))
   expect_snapshot(res |> as.data.frame())
 })
 
-t2 <- t2 |> bold_p(t = 0.10)
-
 test_that("as_tibble works with bold_p()", {
+  t2_regression <- t2_regression |> bold_p(t = 0.10)
+
   bold_p_data <- data.frame(
     x = c(rep("YES", 10), rep("NO", 10)),
     y = c(rep("YES", 9), rep("NO", 11)),
@@ -85,13 +91,13 @@ test_that("as_tibble works with bold_p()", {
   )
 
   expect_equal(
-    as_tibble(t2, col_labels = FALSE)$p.value,
+    as_tibble(t2_regression, col_labels = FALSE)$p.value,
     c("__0.10__", NA, NA, "0.7", ">0.9")
   )
 })
 
 test_that("as_tibble works with formatting functions", {
-  t2_modify_fmt <- t2 |>
+  t2_regression_modify_fmt <- t2_regression |>
     modify_fmt_fun(
       p.value ~ function(x) style_pvalue(x, digits = 3),
       rows = variable == "grade"
@@ -100,18 +106,18 @@ test_that("as_tibble works with formatting functions", {
       estimate ~ function(x) style_ratio(x, digits = 4, decimal.mark = ",")
     )
 
-  expect_silent(res <- as_tibble(t2_modify_fmt))
+  expect_silent(res <- as_tibble(t2_regression_modify_fmt))
   expect_snapshot(res |> as.data.frame())
   expect_equal(
-    as_tibble(t2_modify_fmt, col_labels = FALSE)$p.value,
-    c("__0.10__", NA, NA, "0.688", "0.972")
+    as_tibble(t2_regression_modify_fmt, col_labels = FALSE)$p.value,
+    c("0.10", NA, NA, "0.688", "0.972")
   )
   expect_equal(
-    as_tibble(t2_modify_fmt, col_labels = FALSE)$estimate,
+    as_tibble(t2_regression_modify_fmt, col_labels = FALSE)$estimate,
     c("1,0191", NA, NA, "0,8535", "1,0136")
   )
 
-  t3_modify_fmt <- t3 |>
+  t3_uvregression_modify_fmt <- t3_uvregression |>
     modify_fmt_fun(
       stat_n ~ function(x) style_number(x, digits = 2),
       rows = variable == "age"
@@ -124,34 +130,26 @@ test_that("as_tibble works with formatting functions", {
       c(conf.low, conf.high) ~ styfn_sigfig(digits = 3)
     )
 
-  expect_silent(res <- as_tibble(t3_modify_fmt))
+  expect_silent(res <- as_tibble(t3_uvregression_modify_fmt))
   expect_snapshot(res |> as.data.frame())
   expect_equal(
-    as_tibble(t3_modify_fmt, col_labels = FALSE)$stat_n,
+    as_tibble(t3_uvregression_modify_fmt, col_labels = FALSE)$stat_n,
     c("183.00", "193.0000", NA, NA, NA)
   )
   expect_equal(
-    as_tibble(t3_modify_fmt, col_labels = FALSE)$conf.low,
+    as_tibble(t3_uvregression_modify_fmt, col_labels = FALSE)$conf.low,
     c("0.997, 1.04", NA, NA, "0.446, 2.00", "0.524, 2.29")
   )
 })
 
-t4 <- lm(mpg ~ factor(cyl), mtcars) |>
-  tbl_regression()
-
-t5 <- lm(mpg ~ factor(cyl), mtcars |> dplyr::filter(cyl != 4)) |>
-  tbl_regression()
-
-tbl <- tbl_merge(list(t4, t5))
-
-test_that("as_tibble works with column merging", {
-  expect_silent(res <- as_tibble(tbl))
+test_that("as_tibble works with tbl_merge", {
+  expect_silent(res <- as_tibble(t4_regression_merged))
   expect_snapshot(res |> as.data.frame())
 })
 
 test_that("as_tibble(fmt_missing=) works", {
   expect_silent(
-    res <- tbl |> as_tibble(fmt_missing = TRUE, col_labels = FALSE)
+    res <- t4_regression_merged |> as_tibble(fmt_missing = TRUE, col_labels = FALSE)
   )
   expect_snapshot(res |> as.data.frame())
   expect_equal(
@@ -161,6 +159,12 @@ test_that("as_tibble(fmt_missing=) works", {
   expect_equal(
     res$estimate_2,
     c(NA_character_, NA_character_, "—", "-4.6")
+  )
+  expect_equal(
+    t4_regression_merged |>
+      as_tibble(fmt_missing = FALSE, col_labels = FALSE) |>
+      dplyr::pull(estimate_1),
+    c(NA_character_, NA_character_, "-6.9", "-12")
   )
 
   expect_equal(
