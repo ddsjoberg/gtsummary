8669f162e43a9d2e4fe1e496ac0b5d6b3e48927d
Zelos Zhu <zelos.zhu@atorusresearch.com>
2024-06-11 13:53:22 -0400

    V2.0 add unit tests for `add_n` and `add_nevent` to `tbl_uvregression()`  (#1725)
    * add test for add_n.tbl_uvregression()
    
    * all tests added and styler
    
    * forgot a skip_if_not
    
    * adopt feedback
    
    * adopt feedback

---
+++ tests/testthat/test-add_nevent.tbl_uvregression.R
@@ -0,0 +1,78 @@
+skip_if_not(is_pkg_installed(c("broom.helpers", "broom"), reference_pkg = "gtsummary"))
+
+test_that("add_nevent.tbl_uvregression() works", {
+  expect_silent(
+    tbl <-
+      tbl_uvregression(
+        trial,
+        y = response,
+        method = glm,
+        method.args = list(family = binomial),
+        include = c(trt),
+        exponentiate = TRUE,
+        hide_n = TRUE
+      )
+  )
+  # total N added to table is accurate
+  expect_silent(
+    res <- tbl |> add_nevent()
+  )
+  expect_equal(
+    as.data.frame(res, col_label = FALSE)$stat_nevent |>
+      na.omit() |>
+      unique(),
+    tidyr::drop_na(trial, response, trt) |>
+      dplyr::filter(response == 1) |>
+      nrow() |>
+      as.character()
+  )
+
+  # N added to levels is accurate
+  expect_silent(
+    res <- tbl |> add_nevent(location = "level")
+  )
+  expect_equal(
+    as.data.frame(res, col_label = FALSE)$stat_nevent |>
+      na.omit(),
+    tidyr::drop_na(trial, response, trt) |>
+      dplyr::filter(response == 1) |>
+      with(table(trt)) |>
+      as.integer() |>
+      as.character(),
+    ignore_attr = TRUE
+  )
+
+  # N added to levels and labels is accurate
+  expect_error(
+    res <- tbl |> add_nevent(location = c("label", "level")),
+    NA
+  )
+  expect_equal(
+    as.data.frame(res, col_label = FALSE)$stat_nevent |>
+      na.omit(),
+    tidyr::drop_na(trial, response, trt) |>
+      dplyr::filter(response == 1) |>
+      with(table(trt)) |>
+      as.integer() %>%
+      {c(sum(.), .)} |> # styler: off
+      as.character(),
+    ignore_attr = TRUE
+  )
+})
+
+test_that("add_nevent.tbl_uvregression() messaging for linear model", {
+  expect_silent(
+    tbl1 <- tbl_uvregression(
+      trial,
+      x = trt,
+      include = c(marker, age),
+      show_single_row = trt,
+      method = lm,
+      hide_n = TRUE
+    )
+  )
+  expect_error(
+    res <- tbl1 |> add_nevent(),
+    "Reporting event N on label rows is not available for this model type."
+  )
+})
