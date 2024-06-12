36e59aab6b1661f8e2af962f9a8e7863784b4d87
Daniel Sjoberg <danield.sjoberg@gmail.com>
2024-06-11 21:26:14 -0700

    progress

--- R/utils-survey.R
+++ R/utils-survey.R
@@ -4,3 +4,9 @@
 as.data.frame.survey.design <- function(x, ...) {
   x$variables
 }
+
+#' Test if data is a survey object
+#' @noRd
+is_survey <- function(data) {
+  return(inherits(data, "survey.design") | inherits(data, "svyrep.design"))
+}
