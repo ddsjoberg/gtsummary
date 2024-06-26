2b7735fda09f08e9a17893888510785824b452e4
Daniel Sjoberg <danield.sjoberg@gmail.com>
2024-06-25 19:33:16 -0700

    Adding `tbl_svysummary()` (#1764)
    * progress
    
    * in progress
    
    * in progress
    
    * Update DESCRIPTION
    
    * progress

--- R/assign_summary_digits.R
+++ R/assign_summary_digits.R
@@ -25,9 +25,25 @@
 assign_summary_digits <- function(data, statistic, type, digits = NULL) {
   set_cli_abort_call()
   # stats returned for all variables
-  lst_cat_summary_fns <- .categorical_summary_functions(c("n", "p"))
+  lst_cat_summary_fns <-
+    case_switch(
+      inherits(data, "data.frame") ~ .categorical_summary_functions(c("n", "p")),
+      inherits(data, "survey.design") ~
+        .categorical_summary_functions(c("n", "N", "p",
+                                         "n_unweighted", "N_unweighted", "p_unweighted",
+                                         "p.std.error", "deff"))
+    )
+
   lst_all_fmt_fns <-
-    .categorical_summary_functions(c("N_obs", "N_miss", "N_nonmiss", "p_miss", "p_nonmiss"))
+    case_switch(
+      inherits(data, "data.frame") ~
+        .categorical_summary_functions(c("N_obs", "N_miss", "N_nonmiss", "p_miss", "p_nonmiss")),
+      inherits(data, "survey.design") ~
+        .categorical_summary_functions(c("N_obs", "N_miss", "N_nonmiss", "p_miss", "p_nonmiss",
+                                         "N_obs_unweighted", "N_miss_unweighted", "N_nonmiss_unweighted",
+                                         "p_miss_unweighted", "p_nonmiss_unweighted"))
+    )
+
 
   # extract the statistics
   statistic <- lapply(statistic, function(x) .extract_glue_elements(x) |> unlist())
@@ -69,7 +85,7 @@ assign_summary_digits <- function(data, statistic, type, digits = NULL) {
         return(
           rep_named(
             statistic[[variable]],
-            list(.guess_continuous_summary_digits(data[[variable]]))
+            list(.guess_continuous_summary_digits(data, variable))
           ) |>
             utils::modifyList(lst_all_fmt_fns) |>
             utils::modifyList(digits[[variable]] %||% list())
@@ -98,29 +114,36 @@ assign_summary_digits <- function(data, statistic, type, digits = NULL) {
   )
 }
 
-.guess_continuous_summary_digits <- function(x) {
+.guess_continuous_summary_digits <- function(data, variable) {
   # if all missing, return 0
-  if (all(is.na(x))) {
+  if (all(is.na(as.data.frame(data)[[variable]]))) {
     return(label_style_number(digits = 0L))
   }
 
   # if class is integer, then round everything to nearest integer
-  if (inherits(x, "integer")) {
+  if (inherits(as.data.frame(data)[[variable]], "integer")) {
     return(label_style_number(digits = 0L))
   }
 
   # if it's a date or time, then convert the result to character
-  if (is_date_time(x)) {
+  if (is_date_time(as.data.frame(data)[[variable]])) {
     return(as.character)
   }
 
   # otherwise guess the number of digits to use based on the spread
   # calculate the spread of the variable
-  tryCatch(
-    {
-      var_spread <-
-        stats::quantile(x, probs = c(0.95), na.rm = TRUE) -
-        stats::quantile(x, probs = c(0.05), na.rm = TRUE)
+  # styler: off
+  tryCatch({
+    var_spread <-
+      case_switch(
+        inherits(data, "data.frame") ~
+          stats::quantile(data[[variable]], probs = c(0.95), na.rm = TRUE) -
+          stats::quantile(data[[variable]], probs = c(0.05), na.rm = TRUE),
+        inherits(data, "survey.design") ~
+          cardx::ard_continuous(data, variables = all_of(variable), statistic = ~ c("p5", "p95")) |>
+          dplyr::pull("stat") |>
+          reduce(\(.x, .y) .y - .x)
+      )
 
       label_style_number(
         digits =
@@ -135,23 +158,27 @@ assign_summary_digits <- function(data, statistic, type, digits = NULL) {
     },
     error = function(e) 0L
   )
+  # styler: on
 }
 
-.categorical_summary_functions <-
-  function(statistics = c(
-             "
-                          N", "N_obs", "N_miss", "N_nonmiss", "n_unweighted", "N_unweighted",
-             "p_miss", "p_nonmiss", "p_unweighted"
-           )) {
-    lst_defaults <-
-      c(
-        c("n", "N", "N_obs", "N_miss", "N_nonmiss", "n_unweighted", "N_unweighted") |>
-          intersect(statistics) |>
-          rep_named(list(label_style_number())),
-        c("p", "p_miss", "p_nonmiss", "p_unweighted") |>
-          intersect(statistics) |>
-          rep_named(list(label_style_percent()))
-      )
+.categorical_summary_functions <- function(statistics = c("N", "N_obs", "N_miss", "N_nonmiss",
+                                                          "n_unweighted", "N_unweighted",
+                                                          "p_miss", "p_nonmiss", "p_unweighted")) {
+  lst_defaults <-
+    c(
+      c("n", "N", "N_obs", "N_miss", "N_nonmiss",
+        "N_obs_unweighted", "n_unweighted", "N_unweighted",
+        "N_miss_unweighted", "N_nonmiss_unweighted") |>
+        intersect(statistics) |>
+        rep_named(list(label_style_number())),
+      c("p", "p_miss", "p_nonmiss", "p_unweighted",
+        "p_miss_unweighted", "p_nonmiss_unweighted") |>
+        intersect(statistics) |>
+        rep_named(list(label_style_percent())),
+      c("p.std.error", "deff") |>
+        intersect(statistics) |>
+        rep_named(list(label_style_sigfig(digits = 3)))
+    )
 
-    lst_defaults
-  }
+  lst_defaults
+}
