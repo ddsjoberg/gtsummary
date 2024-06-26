f905bb586eeba7fe07e966c2314ed847ef094630
Daniel Sjoberg <danield.sjoberg@gmail.com>
2024-06-26 14:54:37 -0700

    progress

--- R/add_n.R
+++ R/add_n.R
@@ -55,7 +55,7 @@ add_n <- function(x, ...) {
 #' @name add_n_summary
 #'
 #' @return A table of class `c('tbl_summary', 'gtsummary')`
-#' @examples
+#' @examplesIf gtsummary:::is_pkg_installed(c("survey", "cardx"))
 #' # Example 1 ----------------------------------
 #' trial |>
 #'   tbl_summary(by = trt, include = c(trt, age, grade, response)) |>
