b7b661f9a56d75e7b8547daa3d63307ab5f04640
Zelos Zhu <zelos.zhu@atorusresearch.com>
2024-06-27 17:03:21 +0000

    adopt feedback

--- tests/testthat/test-add_gloabl_p.tbl_uvregression.R
+++ tests/testthat/test-add_gloabl_p.tbl_uvregression.R
@@ -1,24 +1,30 @@
-skip_on_cran()
-test_that("no errors/warnings with standard use after tbl_uvregression", {
-  tbl <- trial %>% tbl_uvregression(method = lm, y = marker, include = c("age", "grade"))
+test_that("add_global_p.tbl_uvregression(x)", {
+  tbl <- trial |> tbl_uvregression(method = lm, y = marker, include = c("age", "grade"))
 
   expect_silent(
-    res <- tbl %>% add_global_p()
+    res <- tbl |> add_global_p()
   )
 
-  expect_snapshot(res %>% as.data.frame())
+  expect_snapshot(res |> as.data.frame())
 
   # two model terms, two p values
   expect_equal(
     sum(!is.na(res$table_body$p.value)),
     2
   )
+
+  # strangely not equal
+  # expect_equal(
+  #   res$table_body$p.value[1:2],
+  #   (car::Anova(lm(marker ~ age + grade, trial)))$`Pr(>F)`
+  # )
+
 })
 
-test_that("testing include argument", {
-  tbl <- trial %>% tbl_uvregression(method = lm, y = marker, include = c("age", "grade"))
+test_that("add_global_p.tbl_uvregression(include)", {
+  tbl <- trial |> tbl_uvregression(method = lm, y = marker, include = c("age", "grade"))
 
-  res1 <- tbl %>% add_global_p(include = age)
+  res1 <- tbl |> add_global_p(include = age)
 
   # one expected p-value for age
   expect_equal(
@@ -27,26 +33,90 @@ test_that("testing include argument", {
   )
 })
 
-test_that("testing type and keep argument", {
-  tbl <- trial %>% tbl_uvregression(method = lm, y = marker, include = c("age", "grade"))
+test_that("add_global_p.tbl_uvregression(type)", {
+  tbl <- trial |> tbl_uvregression(method = lm, y = marker, include = c("age", "grade"))
 
-  res2 <- tbl %>% add_global_p(type = "II", keep = TRUE)
+  res2 <- tbl |> add_global_p(type = "II")
 
   # 4 expected p-values, 2 for each variable, 2 for each level of grade
   expect_equal(
     sum(!is.na(res2$table_body$p.value)),
+    2
+  )
+})
+
+test_that("add_global_p.tbl_uvregression(keep)", {
+  tbl <- trial |> tbl_uvregression(method = lm, y = marker, include = c("age", "grade"))
+
+  res3 <- tbl |> add_global_p(keep = TRUE)
+
+  # 4 expected p-values, 2 for each variable, 2 for each level of grade
+  expect_equal(
+    sum(!is.na(res3$table_body$p.value)),
     4
   )
 })
 
-test_that("testing anova_fun argument", {
-  tbl <- trial %>% tbl_uvregression(method = lm, y = marker, include = c("age", "grade"))
+test_that("add_global_p.tbl_uvregression(anova_fun)", {
+  tbl <- trial |> tbl_uvregression(method = lm, y = marker, include = c("age", "grade"))
 
-  res3 <- tbl %>% add_global_p(anova_fun = cardx::ard_aod_wald_test)
+  res4 <- tbl |> add_global_p(anova_fun = cardx::ard_aod_wald_test)
 
   # two model terms, two p values
   expect_equal(
-    sum(!is.na(res3$table_body$p.value)),
+    sum(!is.na(res4$table_body$p.value)),
     2
   )
+
+  # strangely not equal
+  # expect_equal(
+  #   res4$table_body$p.value[1:2],
+  #   lm(marker ~ age + grade, trial) |>
+  #     cardx::ard_aod_wald_test() |>
+  #     dplyr::filter(variable %in% c("age", "grade") & stat_name == "p.value") |>
+  #     dplyr::pull(stat) %>%
+  #     unlist
+  # )
+})
+
+test_that("add_global_p.tbl_uvregression(anova_fun) inappropriate anova function", {
+  tbl <- trial |> tbl_uvregression(method = lm, y = marker, include = c("age", "grade"))
+
+  broken_anova <- function(x) {
+    x + 1
+  }
+
+  expect_error(
+    res5 <- tbl |> add_global_p(anova_fun = broken_anova),
+    regexp = "There was an error running `anova_fun`"
+  )
+})
+
+test_that("geeglm model for add_global_p.tbl_uvregression()", {
+  suppressMessages(
+    res5 <- geepack::dietox |>
+      dplyr::mutate(Cu = as.factor(Cu)) |>
+      tbl_uvregression(
+        method = geepack::geeglm,
+        y = Weight,
+        include = c("Cu", "Time"),
+        method.args = list(family = poisson("identity"), id = Pig, corstr = "ar1"),
+      ) |>
+      add_global_p()
+  )
+  expect_snapshot(res5 %>% as.data.frame)
+})
+
+test_that("modify tidy_fun to not show p-values", {
+  expect_silent(
+    res6 <- trial |>
+      tbl_uvregression(
+        method = glm,
+        y = response,
+        include = c("age", "grade"),
+        tidy_fun = \(x, ...) broom::tidy(x, ...) |> dplyr::select(-p.value)
+      ) |>
+      add_global_p()
+  )
+  expect_snapshot(res6 %>% as.data.frame)
 })
