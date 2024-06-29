2c703e8136e9f76be5428d3b602b277009f7722c
ddsjoberg <danield.sjoberg@gmail.com>
2024-06-29 15:38:25 +0000

    Deploying to gh-pages from @ ddsjoberg/gtsummary@25965147e066096d70db674763f294bc05a86e4c 🚀

---
+++ dev/reference/libs/tabwid-1.1.3/tabwid.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  var els = document.querySelectorAll(".tabwid");
+  var tabwid_link = document.querySelector('link[href*="tabwid.css"]')
+  if (tabwid_link === null) {
+    const tabwid_styles = document.evaluate("//style[contains(., 'tabwid')]", document, null, XPathResult.ANY_TYPE, null );
+    tabwid_link = tabwid_styles.iterateNext();
+  }
+
+  Array.prototype.forEach.call(els, function(template) {
+      const dest = document.createElement("div");
+      template.parentNode.insertBefore(dest, template.nextSibling)
+      dest.setAttribute("class", "flextable-shadow-host");
+      const fantome = dest.attachShadow({mode: 'open'});
+      fantome.appendChild(template);
+      if (tabwid_link !== null) {
+        fantome.appendChild(tabwid_link.cloneNode(true));
+      }
+  });
+});
+
