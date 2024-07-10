77b449072d7d5439421d36e2cca456b113af1887
Daniel Sjoberg <danield.sjoberg@gmail.com>
2024-07-09 21:46:42 -0700

    Update test-bold_italicize_labels_levels.R

--- tests/testthat/test-bold_italicize_labels_levels.R
+++ tests/testthat/test-bold_italicize_labels_levels.R
@@ -51,3 +51,137 @@ test_that("italicize_levels() works", {
     expr(.data$row_type != "label")
   )
 })
+
+test_that("bold_italicize_labels_levels messaging", {
+  expect_message(
+    testing <- tbl |>
+      modify_table_body(~.x |> dplyr::select(-row_type)) |>
+      bold_labels(),
+    "cannot be used in this context."
+  )
+
+  expect_message(
+    testing <- tbl |>
+      modify_table_body(~.x |> dplyr::select(-row_type)) |>
+      bold_levels(),
+    "cannot be used in this context."
+  )
+
+  expect_message(
+    testing <- tbl |>
+      modify_table_body(~.x |> dplyr::select(-row_type)) |>
+      italicize_labels(),
+    "cannot be used in this context."
+  )
+
+  expect_message(
+    testing <- tbl |>
+      modify_table_body(~.x |> dplyr::select(-row_type)) |>
+      italicize_levels(),
+    "cannot be used in this context."
+  )
+})
+
+# tbl_cross styling ------------------------------------------------------------
+
+test_that("bold_labels.tbl_cross()", {
+  tbl <-
+    tbl_cross(trial, grade, stage) |>
+    bold_labels()
+
+  expect_equal(
+    tbl |>
+      as.data.frame() |>
+      getElement(1L),
+    c("__Grade__", "I", "II", "III", "__Total__")
+  )
+  expect_equal(
+    tbl |>
+      getElement("table_styling") |>
+      getElement("header") |>
+      dplyr::filter(column == "stat_0") |>
+      dplyr::pull(label),
+    "**Total**"
+  )
+  expect_equal(
+    tbl |>
+      getElement("table_styling") |>
+      getElement("header") |>
+      dplyr::filter(startsWith(column, "stat_"), column != "stat_0") |>
+      dplyr::pull(spanning_header) |>
+      unique(),
+    "**T Stage**"
+  )
+})
+
+test_that("bold_levels.tbl_cross()", {
+  tbl <-
+    tbl_cross(trial, grade, stage) |>
+    bold_levels()
+
+  expect_equal(
+    tbl |>
+      as.data.frame() |>
+      getElement(1L),
+    c("Grade", "__I__", "__II__", "__III__", "Total")
+  )
+  expect_equal(
+    tbl |>
+      getElement("table_styling") |>
+      getElement("header") |>
+      dplyr::filter(startsWith(column, "stat_"), column != "stat_0") |>
+      dplyr::pull(label),
+    c("**T1**", "**T2**", "**T3**", "**T4**")
+  )
+})
+
+test_that("italicize_labels.tbl_cross()", {
+  tbl <-
+    tbl_cross(trial, grade, stage) |>
+    italicize_labels()
+
+  expect_equal(
+    tbl |>
+      as.data.frame() |>
+      getElement(1L),
+    c("_Grade_", "I", "II", "III", "_Total_")
+  )
+  expect_equal(
+    tbl |>
+      getElement("table_styling") |>
+      getElement("header") |>
+      dplyr::filter(column == "stat_0") |>
+      dplyr::pull(label),
+    "*Total*"
+  )
+  expect_equal(
+    tbl |>
+      getElement("table_styling") |>
+      getElement("header") |>
+      dplyr::filter(startsWith(column, "stat_"), column != "stat_0") |>
+      dplyr::pull(spanning_header) |>
+      unique(),
+    "*T Stage*"
+  )
+})
+
+test_that("italicize_levels.tbl_cross()", {
+  tbl <-
+    tbl_cross(trial, grade, stage) |>
+    italicize_levels()
+
+  expect_equal(
+    tbl |>
+      as.data.frame() |>
+      getElement(1L),
+    c("Grade", "_I_", "_II_", "_III_", "Total")
+  )
+  expect_equal(
+    tbl |>
+      getElement("table_styling") |>
+      getElement("header") |>
+      dplyr::filter(startsWith(column, "stat_"), column != "stat_0") |>
+      dplyr::pull(label),
+    c("*T1*", "*T2*", "*T3*", "*T4*")
+  )
+})
