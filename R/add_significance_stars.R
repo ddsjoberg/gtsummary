ce90ac60cae5148750ee7443d734a022224bd2a6
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2024-07-24 11:14:59 -0400

    Allow integer class in numeric checks

--- R/add_significance_stars.R
+++ R/add_significance_stars.R
@@ -82,7 +82,7 @@ add_significance_stars <- function(x,
   # checking inputs ------------------------------------------------------------
   check_not_missing(x)
   check_class(x, "gtsummary")
-  check_class(thresholds, "numeric")
+  check_class(thresholds, c("numeric", "integer"))
   check_range(thresholds, range = c(0, 1), include_bounds = c(TRUE, TRUE))
   check_scalar_logical(hide_ci)
   check_scalar_logical(hide_p)
--- R/tbl_survfit.R
+++ R/tbl_survfit.R
@@ -194,8 +194,8 @@ tbl_survfit.list <- function(x,
     predicate = \(x) inherits(x, "survfit"),
     error_msg = "The values passed in the {.cls list} from argument {.arg x} must be class {.cls survfit}."
   )
-  check_class(times, "numeric", allow_empty = TRUE)
-  check_class(probs, "numeric", allow_empty = TRUE)
+  check_class(times, c("numeric", "integer"), allow_empty = TRUE)
+  check_class(probs, c("numeric", "integer"), allow_empty = TRUE)
   if (is_empty(times) + is_empty(probs) != 1L) {
     cli::cli_abort(
       "Specify one and only one of arguments {.arg times} and {.arg probs}.",
