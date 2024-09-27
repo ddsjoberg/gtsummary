06f78e86cccfbb368630e875614d8d7ab9c94cb2
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2024-09-27 01:01:07 -0400

    Clean up

--- R/tbl_hierarchical.R
+++ R/tbl_hierarchical.R
@@ -3,7 +3,7 @@
 #' @param include ([`tidy-select`][dplyr::dplyr_tidy_select])\cr
 #'   variables from `hierarchy` on whose levels (prior to nesting by the variable) summary statistics should be
 #'   calculated for. Must include the last element of `hierarchy`.
-#' @param include ([`tidy-select`][dplyr::dplyr_tidy_select])\cr
+#' @param summary_row ([`tidy-select`][dplyr::dplyr_tidy_select])\cr
 #'   variables from `hierarchy` on whose levels (prior to nesting by the variable) a summary row should be displayed.
 #'
 #' @examples
@@ -22,42 +22,139 @@
 #' )
 #'
 #' @export
-tbl_hierarchical <- function(data, # works
-                             hierarchies, # works
-                             by = NULL,
-                             id = NULL, # works
+tbl_hierarchical <- function(data,
+                             hierarchies,
+                             by = dplyr::group_vars(data),
+                             id = NULL,
                              label = NULL,
-                             denominator = NULL, # works
-                             include = everything(), # works
-                             statistic = ifelse(!missing(id), "{n} ({p})", "{n}"), # works
+                             denominator = NULL,
+                             include = everything(),
+                             statistic = ifelse(!missing(id), "{n} ({p})", "{n}"),
                              digits = NULL,
-                             summary_row = NULL) { # works
+                             summary_row = NULL) {
   set_cli_abort_call()
 
   # process and check inputs ---------------------------------------------------
   check_not_missing(data)
-  check_data_frame(data)
+  check_not_missing(id)
+  check_not_missing(denominator)
   check_not_missing(hierarchies)
+  check_not_missing(include)
+  check_data_frame(data)
   check_string(statistic)
 
   # evaluate tidyselect
   cards::process_selectors(data, hierarchies = {{ hierarchies }}, id = {{ id }}, by = {{ by }})
-  cards::process_selectors(data[hierarchies], include = {{ include }}, summary_row = {{ summary_row }})
 
-  # if id provided, then check that denominator also provided
-  if (!is_empty(id)) {
-    check_data_frame(
-      denominator,
-      message = "A {.cls data.frame} must be passed in argument {.arg denominator} when argument {.arg id} is supplied."
-    )
-  }
-
-  if (is_empty(id) + is_empty(denominator) == 1L) {
+  # denominator must be a data frame, or integer
+  if (!is.data.frame(denominator) && !is_integerish(denominator)) {
     cli::cli_abort(
-      "Specify both arguments {.arg id} and {.arg denominator}, or neither.",
+      "The {.arg denominator} argument must be a {.cls data.frame} or an {.cls integer}, not {.obj_type_friendly {denominator}}.",
       call = get_cli_abort_call()
     )
   }
+
+  # check the id argument is not empty
+  if (is_empty(id)) {
+    cli::cli_abort("Argument {.arg id} cannot be empty.", call = get_cli_abort_call())
+  }
+
+  # create table ---------------------------------------------------------------
+  internal_tbl_hierarchical(
+    data = data,
+    hierarchies = hierarchies,
+    by = by,
+    id = id,
+    label = label,
+    denominator = denominator,
+    include = include,
+    statistic = statistic,
+    digits = digits,
+    summary_row = summary_row
+  )
+}
+
+#' Create hierarchical table
+#'
+#' @param include ([`tidy-select`][dplyr::dplyr_tidy_select])\cr
+#'   variables from `hierarchy` on whose levels (prior to nesting by the variable) summary statistics should be
+#'   calculated for. Must include the last element of `hierarchy`.
+#' @param summary_row ([`tidy-select`][dplyr::dplyr_tidy_select])\cr
+#'   variables from `hierarchy` on whose levels (prior to nesting by the variable) a summary row should be displayed.
+#'
+#' @examples
+#' data <- cards::ADAE |>
+#'   dplyr::filter(
+#'     AESOC %in% unique(cards::ADAE$AESOC)[1:5],
+#'     AETERM %in% unique(cards::ADAE$AETERM)[1:5]
+#'   )
+#'
+#' tbl_hierarchical_count(
+#'   data = data,
+#'   hierarchies = c(AESOC, AETERM, AESEV),
+#'   by = TRTA,
+#'   denominator = cards::ADSL %>% mutate(TRTA = ARM)
+#' )
+#'
+#' @export
+tbl_hierarchical_count <- function(data, # works
+                                   hierarchies, # works
+                                   by = dplyr::group_vars(data),
+                                   label = NULL,
+                                   denominator = NULL, # works
+                                   include = everything(), # works
+                                   digits = NULL,
+                                   summary_row = NULL) { # works
+  set_cli_abort_call()
+
+  # process and check inputs ---------------------------------------------------
+  check_not_missing(data)
+  check_not_missing(hierarchies)
+  check_not_missing(include)
+  check_data_frame(data)
+  cards::process_selectors(data, hierarchies = {{ hierarchies }}, by = {{ by }})
+
+  # denominator must be empty, a data frame, or integer
+  if (!is_empty(denominator) && !is.data.frame(denominator) && !is_integerish(denominator)) {
+    cli::cli_abort(
+      "The {.arg denominator} argument must be empty, a {.cls data.frame}, or an {.cls integer}, not {.obj_type_friendly {denominator}}.",
+      call = get_cli_abort_call()
+    )
+  }
+
+  # create table ---------------------------------------------------------------
+  internal_tbl_hierarchical(
+    data = data,
+    hierarchies = hierarchies,
+    by = by,
+    id = NULL,
+    label = label,
+    denominator = denominator,
+    include = include,
+    statistic = "{ n }",
+    digits = digits,
+    summary_row = summary_row
+  )
+}
+
+internal_tbl_hierarchical <- function(data, # works
+                                      hierarchies, # works
+                                      by = dplyr::group_vars(data),
+                                      id = NULL, # works
+                                      label = NULL,
+                                      denominator = NULL, # works
+                                      include = everything(), # works
+                                      statistic = NULL, # works
+                                      digits = NULL,
+                                      summary_row = NULL) {
+  # process and check inputs ---------------------------------------------------
+  check_not_missing(data)
+  check_not_missing(hierarchies)
+  check_not_missing(include)
+  check_data_frame(data)
+
+  # evaluate tidyselect
+  cards::process_selectors(data[hierarchies], include = {{ include }}, summary_row = {{ summary_row }})
 
   # check that neither 'hierarchies' nor 'include' is empty
   if (is_empty(hierarchies) || is_empty(include)) {
@@ -82,10 +179,10 @@ tbl_hierarchical <- function(data, # works
   }
 
   # check that the last hierarchy level is in either 'include' or 'summary_row'
-  if (!tail(hierarchies, 1) %in% intersect(include, summary_row)) {
+  if (!tail(hierarchies, 1) %in% union(include, summary_row)) {
     cli::cli_abort(
       message = c(
-        "The final variable of {.arg hierarchies} must be included in either {.arg include} or {arg.summary_row},",
+        "The final variable of {.arg hierarchies} must be included in either {.arg include} or {.arg summary_row},",
         "otherwise this variable should be excluded from {.arg hierarchies} altogether.")
     )
   }
@@ -98,35 +195,21 @@ tbl_hierarchical <- function(data, # works
 
   type <- assign_summary_type(data, union(by, hierarchies), value = NULL)
 
-  statistic <- as.list(rep(statistic, length(hierarchies))) |> setNames(hierarchies)
+  if (!is_empty(statistic)) statistic <- as.formula(sprintf("everything() ~ \"%s\"", statistic))
   vars <- if (length(hierarchies) < 2) hierarchies else head(hierarchies, -1)
 
-  # calculate statistics -------------------------------------------------------
-  cards <- cards::ard_stack_hierarchical(
+  cards <- .run_tbl_hierarchical_fun(
     data = data,
-    variables = vars,
-    by = c(by, setdiff(hierarchies, vars)),
+    hierarchies = hierarchies,
+    by = by,
     denominator = denominator,
-    id = all_of(id),
-    overall = TRUE,
-    over_variables = TRUE
+    id = id,
+    vars = vars,
+    summary_row = summary_row
   )
-
-  # calculate summary row statistics -------------------------------------------
-  which_summarize <- c(hierarchies %in% summary_row, FALSE)
-  cards_summary <- if (!is_empty(summary_row)) {
-    cards::ard_stack_hierarchical(
-      data = data,
-      variables = head(hierarchies, -1),
-      by = by,
-      denominator = denominator,
-      id = all_of(id)
-    ) |>
-      dplyr::filter(variable %in% c(by, hierarchies)[which_summarize]) |>
-      .add_gts_column_to_cards_summary(head(hierarchies, -1), by, hierarchical = TRUE)
-  } else {
-    NULL
-  }
+  cards_summary <- cards[[2]]
+  cards <- cards[[1]]
+  func <- if (!is_empty(id)) "tbl_hierarchical" else "tbl_hierarchical_count"
 
   # evaluate the remaining list-formula arguments ------------------------------
   # processed arguments are saved into this env
@@ -134,7 +217,7 @@ tbl_hierarchical <- function(data, # works
     data = scope_table_body(.list2tb(type, "var_type"), data[hierarchies]),
     statistic =
       case_switch(
-        missing(statistic) ~ get_theme_element("tbl_hierarchical-arg:statistic", default = statistic),
+        missing(statistic) ~ get_theme_element(paste0(func, "-arg:statistic"), default = statistic),
         .default = statistic
       ),
     include_env = TRUE
@@ -147,7 +230,7 @@ tbl_hierarchical <- function(data, # works
     scope_table_body(.list2tb(type, "var_type"), data[hierarchies]),
     label =
       case_switch(
-        missing(label) ~ get_deprecated_theme_element("tbl_hierarchical-arg:label", default = label),
+        missing(label) ~ get_deprecated_theme_element(paste0(func, "-arg:label"), default = label),
         .default = label
       )
   )
@@ -156,7 +239,7 @@ tbl_hierarchical <- function(data, # works
     scope_table_body(.list2tb(type, "var_type"), data[hierarchies]),
     digits =
       case_switch(
-        missing(digits) ~ get_theme_element("tbl_hierarchical-arg:digits", default = digits),
+        missing(digits) ~ get_theme_element(paste0(func, "-arg:digits"), default = digits),
         .default = digits
       )
   )
@@ -164,10 +247,14 @@ tbl_hierarchical <- function(data, # works
   # fill in unspecified variables
   cards::fill_formula_selectors(
     scope_table_body(.list2tb(type, "var_type"), data[hierarchies]),
-    statistic =
-      get_theme_element("tbl_hierarchical-arg:statistic", default = eval(formals(gtsummary::tbl_hierarchical)[["statistic"]])),
-    digits =
-      get_theme_element("tbl_hierarchical-arg:digits", default = eval(formals(gtsummary::tbl_hierarchical)[["digits"]]))
+    statistic = get_theme_element(
+      paste0(func, "-arg:statistic"),
+      default = eval(formals(asNamespace("gtsummary")[[func]])[["statistic"]])
+    ),
+    digits = get_theme_element(
+      paste0(func, "-arg:digits"),
+      default = eval(formals(asNamespace("gtsummary")[[func]])[["digits"]])
+    )
   )
 
   # fill each element of digits argument
@@ -202,7 +289,6 @@ tbl_hierarchical <- function(data, # works
     hierarchies,
     type,
     by,
-    id,
     include,
     statistic,
     cards_summary,
@@ -214,5 +300,64 @@ tbl_hierarchical <- function(data, # works
         inputs = tbl_hierarchical_inputs
       )
     ) |>
-    structure(class = c("tbl_hierarchical", "gtsummary"))
+    structure(class = c(func, "gtsummary"))
+}
+
+# this function calculates either the counts or the rates of the events
+.run_tbl_hierarchical_fun <- function(data, hierarchies, by, denominator, id, vars, summary_row) {
+  # calculate statistics -------------------------------------------------------
+  if (!is_empty(id)) {
+    cards <- cards::ard_stack_hierarchical(
+      data = data,
+      variables = vars,
+      by = c(by, setdiff(hierarchies, vars)),
+      denominator = denominator,
+      id = all_of(id),
+      overall = TRUE,
+      over_variables = TRUE
+    )
+
+    # calculate summary row statistics -------------------------------------------
+    which_summarize <- c(hierarchies %in% summary_row, FALSE)
+    cards_summary <- if (!is_empty(summary_row)) {
+      cards::ard_stack_hierarchical(
+        data = data,
+        variables = head(hierarchies, -1),
+        by = by,
+        denominator = denominator,
+        id = all_of(id)
+      ) |>
+        dplyr::filter(variable %in% c(by, hierarchies)[which_summarize]) |>
+        .add_gts_column_to_cards_summary(head(hierarchies, -1), by, hierarchical = TRUE)
+    } else {
+      NULL
+    }
+  } else {
+    # calculate statistics -------------------------------------------------------
+    cards <- cards::ard_stack_hierarchical_count(
+      data = data,
+      variables = vars,
+      by = c(by, setdiff(hierarchies, vars)),
+      denominator = denominator,
+      overall = TRUE,
+      over_variables = TRUE
+    )
+
+    # calculate summary row statistics -------------------------------------------
+    which_summarize <- c(hierarchies %in% summary_row, FALSE)
+    cards_summary <- if (!is_empty(summary_row)) {
+      cards::ard_stack_hierarchical_count(
+        data = data,
+        variables = head(hierarchies, -1),
+        by = by,
+        denominator = denominator
+      ) |>
+        dplyr::filter(variable %in% c(by, hierarchies)[which_summarize]) |>
+        .add_gts_column_to_cards_summary(head(hierarchies, -1), by, hierarchical = TRUE)
+    } else {
+      NULL
+    }
+  }
+
+  list(cards = cards, cards_summary = cards_summary)
 }
