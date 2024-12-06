ee4214a09b99a05e2b4c8a62b5a3dd962c64e228
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2024-12-05 21:51:47 -0500

    Add sorting function

---
+++ R/sort_tbl_hierarchical.R
@@ -0,0 +1,196 @@
+#' Sort Hierarchical Tables
+#'
+#' @description `r lifecycle::badge('experimental')`\cr
+#'
+#' This function is used to sort hierarchical tables. Options for sorting criteria are:
+#'
+#' 1. Alphanumeric - rows are ordered alphanumerically by label text (default).
+#' 2. Frequency - within each section of the hierarchy table, frequency sums are calculated for each row and rows are
+#'    ordered accordingly.
+#'
+#' @param x (`tbl_hierarchical`)\cr
+#'   A hierarchical gtsummary table of class `'tbl_hierarchical'`.
+#' @param sort (`string`)\cr
+#'   Specifies sorting to perform. Values must be one of `c("alphanumeric", "frequency")`. Default is `"frequency"`.
+#' @param ascending (`logical`)\cr
+#'   Whether to sort rows in ascending or descending order. Default is descending (`ascending = FALSE`).
+#' @param .stat (`string`)\cr
+#'   Statistic to use for sorting when `sort = "frequency"`. This statistic must be present in the table for all
+#'   hierarchy levels.
+#'
+#' @name sort_tbl_hierarchical
+#'
+#' @examples
+#' ADAE_subset <- cards::ADAE |>
+#'   dplyr::filter(
+#'     AESOC %in% unique(cards::ADAE$AESOC)[1:5],
+#'     AETERM %in% unique(cards::ADAE$AETERM)[1:5]
+#'   )
+#'
+#' tbl <- tbl_hierarchical(
+#'   data = ADAE_subset,
+#'   variables = c(SEX, RACE, AETERM),
+#'   by = TRTA,
+#'   denominator = cards::ADSL |> mutate(TRTA = ARM),
+#'   id = USUBJID,
+#'   include = AETERM,
+#'   overall_row = TRUE
+#' )
+#'
+#' tbl_sort(tbl, .stat = "N")
+NULL
+
+#' @rdname sort_tbl_hierarchical
+#' @export
+tbl_sort <- function(x, ...) {
+  check_not_missing(x)
+  check_class(x, "gtsummary")
+
+  UseMethod("tbl_sort")
+}
+
+#' @rdname sort_tbl_hierarchical
+#' @export
+tbl_sort.tbl_hierarchical <- function(x, sort = "frequency", ascending = FALSE, .stat = "n") {
+  set_cli_abort_call()
+
+  # process and check inputs ---------------------------------------------------
+  check_logical(ascending)
+
+  if (!sort %in% c("frequency", "alphanumeric")) {
+    cli::cli_abort(
+      "The {.arg sort} argument must be either {.val frequency} or {.val alphanumeric}.",
+      call = get_cli_abort_call()
+    )
+  }
+
+  if (sort == "alphanumeric") {
+    sort_cols <- c(
+      x$table_body |> select(cards::all_ard_groups("names")) |> names(),
+      "label"
+    )
+
+    x$table_body <- x$table_body |>
+      arrange(across(sort_cols, ~ if (!ascending) desc(.x) else .x))
+  } else {
+    x <- .append_sort_counts(x, .stat)
+    u_cols <- x$table_body |> select(cards::all_ard_groups("names")) |> unlist() |> unique()
+    g_cols <- sapply(x$table_body |> select(cards::all_ard_groups("names")), function(x) tail(unique(na.omit(x)), 1))
+
+    # assign counts for each hierarchy level
+    for (g in names(g_cols)) {
+      x$table_body <- x$table_body |>
+        dplyr::group_by(across(c(g, paste0(g, "_level"))), .add = TRUE)
+
+      x$table_body <- x$table_body |>
+        left_join(
+          summarise(x$table_body, !!paste0("count_", g) := first(count_total)),
+          by = x$table_body |> dplyr::group_vars()
+        )
+    }
+    x$table_body <- x$table_body |>
+      dplyr::rowwise() |>
+      mutate(inner_var = if (.data$variable %in% u_cols) " " else .data$variable) |>
+      dplyr::ungroup()
+
+    # sort by counts ------------------------------------------------------------------------------
+    sort_cols <- c(rbind(
+      x$table_body |> select(cards::all_ard_groups("names")) |> names(),
+      x$table_body |> select(starts_with("count_group")) |> names(),
+      x$table_body |> select(cards::all_ard_groups("levels")) |> names()
+    ), "inner_var", "count_total", "label")
+
+    x$table_body <- x$table_body |>
+      arrange(across(sort_cols, ~ if (is.numeric(.x) && !ascending) desc(.x) else .x))
+  }
+
+  x
+}
+
+.append_sort_counts <- function(x, .stat) {
+  if (!.stat %in% x$cards$tbl_hierarchical$stat_name) {
+    cli::cli_abort(
+      "The {.arg .stat} argument is {.val {(.stat)}} but this statistic is not present in {.arg x}. For all valid
+      statistic options see the {.val stat_name} column of {.code x$cards$tbl_hierarchical}.",
+      call = get_cli_abort_call()
+    )
+  }
+
+  cards <- x$cards$tbl_hierarchical
+  by_cols <- if (ncol(x$table_body |> select(starts_with("stat_"))) > 1) c("group1", "group1_level") else NA
+  g_cols <- sapply(x$table_body |> select(cards::all_ard_groups("names")), function(x) tail(unique(na.omit(x)), 1))
+
+  # fill in variable_level column of cards
+  cards$variable_level[cards$variable == "..ard_hierarchical_overall.."] <- x$table_body |>
+    dplyr::filter(variable == "..ard_hierarchical_overall..") |>
+    dplyr::pull("label") |>
+    as.list()
+
+  # extract counts ------------------------------------------------------------------------------
+  cards <- cards |>
+    dplyr::filter(stat_name == .stat, variable %in% x$table_body$variable) |>
+    dplyr::group_by(across(c(cards::all_ard_groups(), cards::all_ard_variables(), -by_cols))) |>
+    dplyr::summarise(count_total = sum(unlist(stat))) |>
+    dplyr::ungroup() |>
+    dplyr::rename(label = variable_level) |>
+    tidyr::unnest(cols = everything())
+
+  # match names to x$table_body
+  if (length(by_cols) > 1) {
+    names(cards)[grep("group", names(cards))] <- x$table_body |>
+      select(cards::all_ard_groups()) |>
+      names()
+  }
+  cards[cards$variable == "..ard_hierarchical_overall..", 1] <- "..ard_hierarchical_overall.."
+
+  # align cards layout with x$table_body --------------------------------------------------------
+  cards <- cards |>
+    dplyr::rowwise() |>
+    dplyr::mutate(across(
+      cards::all_ard_groups(),
+      ~ if (is.na(.x) && !grepl("_level", cur_column()) && variable == g_cols[cur_column()]) {
+        variable
+      } else if (is.na(.x) && variable %in% g_cols[gsub("_level", "", cur_column())]) {
+        label
+      } else {
+        .x
+      }
+    ))
+
+  # calculate total group sums for any variables not in include ---------------------------------
+  if (!all(g_cols %in% cards$variable)) {
+    gp_vars <- g_cols[g_cols %in% setdiff(g_cols, cards$variable)]
+    cli::cli_warn(
+      "Not all hierarchy variables present in the table were included in the {.arg include} argument.
+      These variables ({gp_vars}) do not have event rate data available so the total sum of the event
+      rates for this hierarchy section will be used instead. To use event rates to sort all sections of the table,
+      set {.code include = everything()} when creating your table via {.fun tbl_hierarchical}.",
+      call = get_cli_abort_call()
+    )
+
+    gp_cols <- names(gp_vars)
+    for (i in seq_along(gp_cols)) {
+      cards <- cards |>
+        dplyr::bind_rows(
+          cards |>
+            filter(variable != "..ard_hierarchical_overall..") |>
+            group_by(across(c(gp_cols[1:i], paste0(gp_cols[1:i], "_level")))) |>
+            summarize(count_total = sum(count_total)) |>
+            mutate(
+              variable = .data[[gp_cols[i]]],
+              label = .data[[paste0(gp_cols[i], "_level")]]
+            )
+        )
+    }
+  }
+
+  # add counts to x$table_body ------------------------------------------------------------------
+  x$table_body <- x$table_body |>
+    dplyr::left_join(
+      cards,
+      by = c(cards |> select(-"count_total") |> names())
+    ) |>
+    dplyr::mutate(across(cards::all_ard_groups(), .fns = ~tidyr::replace_na(., " ")))
+
+  x
+}
