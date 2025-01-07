9f5f45f5a2cae589062e287714bb2ec27eb50cfa
Abinaya Yogasekaram <73252787+ayogasekaram@users.noreply.github.com>
2025-01-06 19:42:38 -0500

    tbl_ard*() article draft (#2082)
    * tbl_ard*() article draft
    
    * address comments from review
    
    * updates for article and other functions
    
    * merge updates
    
    ---------
    
    Co-authored-by: Daniel Sjoberg <danield.sjoberg@gmail.com>

--- R/utils-gtsummary_core.R
+++ R/utils-gtsummary_core.R
@@ -1,110 +1,20 @@
-# .create_gtsummary_object -----------------------------------------------------
+#' Create gtsummary table
+#'
+#' USE `as_gtsummary()` INSTEAD!
+#' This function ingests a data frame and adds the infrastructure around it
+#' to make it a gtsummary object.
+#'
 #' Function uses `table_body` to create a gtsummary object
 #'
-#' @param table_body the table_body tibble
-#' @param ... other objects that will be added to the gtsummary object list
-#'
+#' @inheritParams as_gtsummary
 #' @export
 #' @keywords internal
 #' @return gtsummary object
 .create_gtsummary_object <- function(table_body, ...) {
-  x <- list() # empty gtsummary object
-
-  # table_body -----------------------------------------------------------------
-  x$table_body <- table_body
-
-  # table_styling --------------------------------------------------------------
-  x$table_styling$header <-
-    dplyr::tibble(
-      column = names(x$table_body),
-      hide = TRUE,
-      align = "center",
-      interpret_label = "gt::md",
-      label = names(x$table_body)
-    ) %>%
-    dplyr::mutate(
-      hide = ifelse(.data$column %in% "label", FALSE, .data$hide),
-      align = ifelse(.data$column %in% "label", "left", .data$align)
-    )
-
-  x$table_styling$spanning_header <-
-    dplyr::tibble(
-      level = integer(),
-      column = character(),
-      spanning_header = character(),
-      text_interpret = character(),
-      remove = logical()
-    )
-
-  x$table_styling$footnote_header <-
-    dplyr::tibble(
-      column = character(),
-      footnote = character(), text_interpret = character(),
-      replace = logical(), remove = logical()
-    )
-
-  x$table_styling$footnote_body <-
-    dplyr::tibble(
-      column = character(), rows = list(),
-      footnote = character(), text_interpret = character(),
-      replace = logical(), remove = logical()
-    )
-
-  x$table_styling$footnote_spanning_header <-
-    dplyr::tibble(
-      column = character(), footnote = character(),
-      level = integer(), text_interpret = character(),
-      replace = logical(), remove = logical()
-    )
-
-  x$table_styling$abbreviation <-
-    dplyr::tibble(
-      column = character(),
-      abbreviation = character(),
-      text_interpret = character()
-    )
-
-  x$table_styling$source_note <-
-    dplyr::tibble(
-      id = integer(),
-      source_note = character(),
-      text_interpret = character(),
-      remove = logical()
-    )
-
-  x$table_styling$text_format <-
-    dplyr::tibble(
-      column = character(), rows = list(),
-      format_type = character(), undo_text_format = logical()
-    )
-
-  x$table_styling$indent <-
-    # if there is a label column, make it indent 0 (which makes it easier to modify later)
-    if ("label" %in% x$table_styling$header$column) {
-      dplyr::tibble(
-        column = "label",
-        rows = list(rlang::expr(TRUE)),
-        n_spaces = 0L
-      )
-    } else {
-      dplyr::tibble(column = character(), rows = list(), n_spaces = integer())
-    }
-
-  x$table_styling$fmt_missing <-
-    dplyr::tibble(column = character(), rows = list(), symbol = character())
-  x$table_styling$fmt_fun <-
-    dplyr::tibble(column = character(), rows = list(), fmt_fun = list())
-  x$table_styling$cols_merge <-
-    dplyr::tibble(column = character(), rows = list(), pattern = character())
-
-  # adding other objects to list -----------------------------------------------
-  x <- c(x, list(...))
-
-  # returning gtsummary object -------------------------------------------------
-  x |>
-    structure(class = "gtsummary")
+  as_gtsummary(table_body, ...) |>
+    # the original function left the "label" column unhidden
+    modify_column_hide(columns = -any_of("label"))
 }
-
 
 .purrr_when <- function(...) {
   lst_formulas <- rlang::dots_list(...)
