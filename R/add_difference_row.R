30c79113d71b6072a51ef4edad43b58b2f24cc4c
Daniel D. Sjoberg <danield.sjoberg@gmail.com>
2025-04-29 15:31:17 -0700

    in progress

--- R/add_difference_row.R
+++ R/add_difference_row.R
@@ -1,16 +1,52 @@
-
+#' Add difference rows
+#'
+#' - [`add_difference_row.tbl_summary()`]
+#'
+#' @param x (`gtsummary`)\cr
+#'   Object with class 'gtsummary'
+#' @param ... Passed to other methods.
+#' @keywords internal
+#'
+#' @author Daniel D. Sjoberg
+#' @export
 add_difference_row <- function(x, ...) {
   check_not_missing(x)
   check_class(x, "gtsummary")
   UseMethod("add_difference_row")
 }
 
-
+#' Add differences rows between groups
+#'
+#' @description
+#' `r lifecycle::badge('experimental')`\cr
+#' Adds difference to tables created by [`tbl_summary()`] as additional rows.
+#' This function is often useful compared to `add_difference()` when there are
+#' more than two groups to compare.
+#'
+#' Pairwise differences are calculated relative to the specified `by` variable's reference level.
+#'
+#' @inheritParams add_difference.tbl_summary
+#' @param reference (scalar)\cr
+#'   Value of the `tbl_summary(by)` variable value that is the reference for
+#'   each of the difference calculations.
+#' @hea
+#' @export
+#' @return a gtsummary table of class `"tbl_summary"`
+#'
+#' @examplesIf (identical(Sys.getenv("NOT_CRAN"), "true") || identical(Sys.getenv("IN_PKGDOWN"), "true")) && gtsummary:::is_pkg_installed("cardx") && gtsummary:::is_pkg_installed("broom", ref = "cardx")
+#' # Example 1 ----------------------------------
+#' trial |>
+#'   tbl_summary(by = grade, include = c(age, response), missing = "no") |>
+#'   add_difference_row(
+#'     reference = "I",
+#'     statistic = everything() ~ c("{estimate}", "{conf.low}, {conf.high}", "{p.value}")
+#'   )
 add_difference_row.tbl_summary <- function(x,
                                            reference,
-                                           statistic,
+                                           statistic = everything() ~ "{estimate}",
                                            test = NULL,
                                            group = NULL,
+                                           header = NULL,
                                            adj.vars = NULL,
                                            test.args = NULL,
                                            conf.level = 0.95,
@@ -22,12 +58,11 @@ add_difference_row.tbl_summary <- function(x,
                                              all_tests("smd") ~ label_style_sigfig()
                                            ),
                                            ...) {
-  # check intputs --------------------------------------------------------------
+  # check inputs ---------------------------------------------------------------
   set_cli_abort_call()
   check_dots_empty(call = get_cli_abort_call())
   updated_call_list <- c(x$call_list, list(add_difference = match.call()))
   check_not_missing(reference)
-  check_not_missing(statistic)
   check_scalar(reference)
 
   # checking that input x has a by var and it has two levels
@@ -39,7 +74,6 @@ add_difference_row.tbl_summary <- function(x,
   # if `pvalue_fun` not modified, check if we need to use a theme p-value
   if (missing(pvalue_fun)) {
     pvalue_fun <-
-      get_deprecated_theme_element("add_p.tbl_summary-arg:pvalue_fun") %||%
       get_theme_element("pkgwide-fn:pvalue_fun") %||%
       pvalue_fun
   }
@@ -54,7 +88,7 @@ add_difference_row.tbl_summary <- function(x,
   if (!x$inputs$percent %in% "column" &&
       any(unlist(x$inputs$type[include]) %in% c("categorical", "dichotomous"))) {
     cli::cli_warn(c(
-      "The {.code add_difference()} results for categorical variables may not
+      "The {.code add_difference_row()} results for categorical variables may not
        compatible with {.code tbl_summary(percent = c('cell', 'row'))}.",
       i = "Use column percentages instead, {.code tbl_summary(percent = 'column')}."
     ))
@@ -83,16 +117,75 @@ add_difference_row.tbl_summary <- function(x,
         "i" = "For example, {.code everything() ~ c('{{estimate}}', '{{conf.low}}, {{conf.high}}', '{{p.value}}')}"),
   )
 
-  # TODO: Assign a default test
-  test = rep_named(include, list("t.test"))
+  # select test ----------------------------------------------------------------
+  test <-
+    assign_tests(
+      x = x,
+      test = test,
+      group = group,
+      adj.vars = adj.vars,
+      include = include,
+      calling_fun = "add_difference"
+    )
+
+  # add all available test meta data to a data frame ---------------------------
+  df_test_meta_data <-
+    imap(
+      test,
+      ~ dplyr::tibble(variable = .y, fun_to_run = list(.x), test_name = attr(.x, "test_name") %||% NA_character_)
+    ) |>
+    dplyr::bind_rows()
+
+  # add test names to `.$table_body` so it can be used in selectors ------------
+  if (!"test_name" %in% names(x$table_body)) {
+    x$table_body <-
+      dplyr::left_join(
+        x$table_body,
+        df_test_meta_data[c("variable", "test_name")],
+        by = "variable"
+      ) |>
+      dplyr::relocate("test_name", .after = "variable")
+  } else {
+    x$table_body <-
+      dplyr::rows_update(
+        x$table_body,
+        df_test_meta_data[c("variable", "test_name")],
+        by = "variable",
+        unmatched = "ignore"
+      ) |>
+      dplyr::relocate("test_name", .after = "variable")
+  }
+
+  # now process the `test.args` and `estimate_fun` arguments -------------------
+  cards::process_formula_selectors(
+    scope_table_body(x$table_body, x$inputs$data[include]),
+    estimate_fun = estimate_fun
+  )
+  # fill in unspecified variables
+  cards::fill_formula_selectors(
+    scope_table_body(x$table_body, x$inputs$data[include]),
+    estimate_fun = eval(formals(asNamespace("gtsummary")[["add_difference.tbl_summary"]])[["estimate_fun"]])
+  )
+
+  cards::process_formula_selectors(
+    scope_table_body(x$table_body, x$inputs$data[include]),
+    test.args = test.args
+  )
+  cards::check_list_elements(
+    test.args,
+    predicate = \(x) is.list(x) && is_named(x),
+    error_msg = c("Error in the argument {.arg {arg_name}} for variable {.val {variable}}.",
+                  i = "Value must be a named list."
+    )
+  )
 
   # check reference level is appropriate
   lst_by_levels <-
     x$table_styling$header |>
-    dplyr::filter(startsWith(.data$column, "stat_")) |>
+    dplyr::filter(grepl(pattern = "^stat_\\d*[1-9]\\d*$", x = .data$column)) |>
     dplyr::select("column", "modify_stat_level") |>
     deframe() |>
-    as.list()
+    lapply(FUN = as.character)
   if (!as.character(reference) %in% unlist(lst_by_levels)) {
     cli::cli_abort(
       "The {.arg reference} argument must be one of {.val {unlist(lst_by_levels)}}.",
@@ -100,6 +193,230 @@ add_difference_row.tbl_summary <- function(x,
     )
   }
 
-  # prep data for tests
+  # prep data for tests, by adding reference level to the first position in factor
+  data <- x$inputs$data
   data[[x$inputs$by]] <- forcats::fct_relevel(data[[x$inputs$by]], reference, after = 0L)
+
+  # create data frame that is one line per test to be calculated
+  df_results <-
+    tidyr::expand_grid(
+      variable = include,
+      reference_level = reference,
+      compare_level = unlist(lst_by_levels) |> setdiff(reference)
+    ) |>
+    # merge in table_body column name and subsetted data frame
+    dplyr::left_join(
+      enframe(unlist(lst_by_levels), "column", "compare_level") |>
+        dplyr::filter(!.data$compare_level %in% .env$reference) |>
+        dplyr::mutate(
+          data =
+            map(
+              .data$compare_level,
+              \(.x, .y) {
+                .env$data |>
+                  dplyr::filter(.data[[x$inputs$by]] %in% c(.x, .env$reference)) |>
+                  dplyr::mutate("{x$inputs$by}" := factor(.data[[x$inputs$by]])) # this removes unobserved levels of a factor
+              }
+            )
+        ),
+      by = "compare_level"
+    )
+
+  df_results$result <-
+    pmap(
+      list(df_results$variable,
+           df_results$data),
+      \(variable, data) {
+        lst_captured_results <-
+          cards::eval_capture_conditions(
+            do.call(
+              what =
+                df_test_meta_data |>
+                dplyr::filter(.data$variable %in% .env$variable) |>
+                dplyr::pull("fun_to_run") %>%
+                getElement(1),
+              args = list(
+                data = data, # most arg names here are data, but `tbl_survfit(x)` is a list of survfit
+                variable = variable,
+                by = x$inputs$by,
+                group = group,
+                type = tryCatch(x$inputs$type[[variable]], error = \(e) NULL), # in tbl_survfit(), the type argument is for transforming the estimate, not the summary type
+                test.args = test.args[[variable]],
+                adj.vars = adj.vars,
+                conf.level = conf.level
+              )
+            )
+          )
+
+        # if the result is null, replace is with a data frame of of NA
+        chr_expected_stats <- c("estimate", "std.error", "parameter", "statistic", "conf.low", "conf.high", "p.value")
+        if (is.null(lst_captured_results[["result"]])) {
+          rep_named(chr_expected_stats, list(NA)) |>
+            as.data.frame()
+        }
+
+        # check class of returned object. Object must be ARD of class 'card' or a data frame with one row
+        if (!(inherits(lst_captured_results[["result"]], "card") ||
+              (is.data.frame(lst_captured_results[["result"]]) &&
+               nrow(lst_captured_results[["result"]]) == 1L))) {
+          cli::cli_abort(
+            c("The result from the {.arg test} argument for variable {.var {variable}}
+               must be an ARD of class {.cls card} or a data frame with one row,
+               similar to what is returned by {.fun broom::tidy}."),
+            call = get_cli_abort_call
+          )
+        }
+
+        # convert the broom-like data frame to an ARD
+        if (!is.null(lst_captured_results[["result"]]) &&
+            !inherits(lst_captured_results[["result"]], "card")) {
+          lst_captured_results[["result"]] <-
+            lst_captured_results[["result"]] |>
+            cards::ard_identity(variable = .env$variable) |>
+            dplyr::mutate(
+              warning = lst_captured_results["warning"],
+              error = lst_captured_results["error"]
+            )
+        }
+
+        # print captured errors or warnings
+        cards::print_ard_conditions(lst_captured_results[["result"]])
+
+        # Add rounded statistic and return ARD
+        lst_captured_results[["result"]] |>
+          cards::update_ard_fmt_fn(
+            stat_names = chr_expected_stats,
+            fmt_fn = estimate_fun[[variable]]
+          ) |>
+          cards::update_ard_fmt_fn(
+            stat_names = "p.value",
+            fmt_fn = pvalue_fun
+          ) |>
+          cards::apply_fmt_fn()
+      }
+    )
+
+  # create vector of results
+  df_results$result_fmt <-
+    pmap(
+      list(df_results$variable, df_results$result),
+      \(variable, result) {
+        lst_results <-
+          result |>
+          dplyr::filter(map_lgl(.data$stat_fmt, Negate(is.null))) |>
+          cards::get_ard_statistics(.column = "stat_fmt")
+
+        map(
+          statistic[[variable]],
+          ~ glue::glue_data(.x = lst_results, .x)
+        )
+      }
+    )
+
+  # create label for new statistics
+  df_results$result_lbl <-
+    pmap(
+      list(df_results$variable, df_results$result),
+      \(variable, result) {
+        lst_results <-
+          result |>
+          dplyr::filter(map_lgl(.data$stat_fmt, Negate(is.null))) |>
+          cards::get_ard_statistics(.column = "stat_label")
+
+        map(
+          statistic[[variable]],
+          ~ ifelse(
+            .x == "{conf.low}, {conf.high}",
+            glue::glue("{style_number(conf.level, scale = 100)}% CI"), # replace {conf.low}, {conf.high} with "95% CI"
+            glue::glue_data(.x = lst_results, .x)
+          )
+        )
+      }
+    )
+
+  # prep results to place them in table_body
+  df_results_wide <-
+    df_results |>
+    dplyr::left_join(
+      df_test_meta_data[c("variable", "test_name")],
+      by = "variable"
+    ) |>
+    dplyr::select("variable", "test_name", "column", "result_fmt", label = "result_lbl") |>
+    tidyr::unnest(cols = c("result_fmt", "label")) |>
+    dplyr::mutate(across(c("result_fmt", "label"), unlist)) |>
+    tidyr::pivot_wider(
+      id_cols = c("variable", "test_name", "label"),
+      values_from = "result_fmt",
+      names_from = "column"
+    ) |>
+    dplyr::mutate(row_type = "difference_row")
+
+  # get index values where new lines are to be inserted
+  variable_index <-
+    x$table_body |>
+    dplyr::select("variable") |>
+    dplyr::mutate(row_number = dplyr::row_number()) |>
+    dplyr::filter(
+      .by = "variable",
+      dplyr::n() == dplyr::row_number(),
+      .data$variable %in% .env$include
+    ) |>
+    dplyr::slice_tail(by = "variable", n = 1L) |>
+    deframe()
+
+  # add each of the rows to table_body
+  for (v in rev(names(variable_index))) {
+    x$table_body <-
+      x$table_body |>
+      dplyr::add_row(
+        dplyr::bind_rows(
+          if (!is_empty(.env$header)) {
+            data.frame(variable = paste0(v, "-row_difference"), row_type = "label", label = header)
+          },
+          dplyr::filter(df_results_wide, .data$variable == .env$v) |>
+            dplyr::mutate(variable = paste0(.data$variable, "-row_difference"))
+        ),
+        .after = variable_index[[v]]
+      )
+  }
+
+  # prepping ARD to return with result -----------------------------------------
+  card <-
+    df_results |>
+    dplyr::rowwise() |>
+    dplyr::mutate(
+      result_lst =
+        list(.data$result) |>
+        set_names(nm = paste(shQuote(.data$reference_level), shQuote(compare_level), sep = " vs. "))
+    ) |>
+    dplyr::select("variable", "result_lst") |>
+    tidyr::nest(result_nested = "result_lst") |>
+    dplyr::rowwise() |>
+    dplyr::mutate(
+      result_final =
+        list(.data$result_nested[[1]]) |>
+        set_names(nm = .data$variable)
+
+    ) |>
+    dplyr::pull("result_final")
+
+  # add info to table ----------------------------------------------------------
+  x$call_list[["add_difference_row"]] <- match.call()
+  x$cards[["add_difference_row"]] <- card
+
+  # add final styling to table -------------------------------------------------
+  x |>
+    .modify_column_indent(
+      columns = "label",
+      rows = .data$row_type == "difference_row",
+      indent = 4L
+    ) |>
+    .modify_missing_symbol(
+      columns =
+        x$table_styling$header |>
+        dplyr::filter(.data$modify_stat_level == .env$reference) |>
+        dplyr::pull("column"),
+      rows = .data$row_type == "difference_row",
+      symbol = "\U2014"
+    )
 }
