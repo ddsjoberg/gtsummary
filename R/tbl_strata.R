01b2d02574a60ba57c741526959b1156443393f0
Daniel D. Sjoberg <danield.sjoberg@gmail.com>
2025-05-30 08:08:36 -0700

    Update tbl_strata.R

--- R/tbl_strata.R
+++ R/tbl_strata.R
@@ -271,7 +271,7 @@ tbl_strata_internal <- function(data,
   if (.combine_with == "tbl_merge") {
     tbl <- inject(tbl_merge(tbls = df_tbls$tbl, tbl_ids = df_tbls$tbl_id, !!!.combine_args))
   } else if (.combine_with == "tbl_stack") {
-    tbl <- inject(tbl_stack(tbls = df_tbls$tb, tbl_ids = df_tbls$tbl_idl, !!!.combine_args))
+    tbl <- inject(tbl_stack(tbls = df_tbls$tbl, tbl_ids = df_tbls$tbl_id, !!!.combine_args))
   }
 
   # return tbl -----------------------------------------------------------------
