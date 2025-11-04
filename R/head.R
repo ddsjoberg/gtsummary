0bbe94aa610103f88f6ca50a6b5f91b668dc069d
melkiades <davide.garolini@roche.com>
2025-11-04 17:25:32 +0100

    fix rev

--- R/head.R
+++ R/head.R
@@ -1,45 +1,39 @@
-#' Display the first rows of a gtsummary table
+#' Display the first or last rows of a {gtsummary} table
+#'
+#' @description
+#' These functions allow you to view the first (`head()`) or last (`tail()`) `n`
+#' rows of a {gtsummary} table.
 #'
 #' @param x A `gtsummary` object.
-#' @param n Number of rows to return. Default is `6L` as default in `head()`.
-#' @param ... Additional arguments passed to `head()`.
+#' @param n Number of rows to return. Default is `6L` as default in [utils::head()] and
+#'   [utils::tail()].
+#' @param ... Additional arguments passed to [utils::head()] or [utils::tail()].
 #'
-#' @return A `gtsummary` object with only the first `n` rows in `table_body`.
+#' @return A `gtsummary` object with only the first or last `n` rows in `table_body`.
 #'
 #' @examples
+#' # head() example
 #' trial |>
 #'   tbl_summary(by = trt) |>
-#'   tail(n = 2L)
+#'   head(n = 2L)
 #'
+#' @keywords internal
 #' @export
 head.gtsummary <- function(x, n = 6L, ...) {
-  set_cli_abort_call()
-  if (!inherits(x, "gtsummary")) {
-    cli::cli_abort("{.arg x} must be a gtsummary object.", call = get_cli_abort_call())
-  }
   x$table_body <- head(x$table_body, n = n, ...) # data.frame head
   x
 }
 
-#' Display the last rows of a gtsummary table
-#'
-#' @param x A `gtsummary` object.
-#' @param n Number of rows to return. Default is `6L` as default in `tail()`.
-#' @param ... Additional arguments passed to `tail()`.
-#'
-#' @return A `gtsummary` object with only the last `n` rows in `table_body`.
-#'
 #' @examples
+#' # tail() example
 #' trial |>
 #'   tbl_summary(by = trt) |>
 #'   tail(n = 2L)
 #'
+#' @rdname head.gtsummary
+#' @keywords internal
 #' @export
 tail.gtsummary <- function(x, n = 6L, ...) {
-  set_cli_abort_call()
-  if (!inherits(x, "gtsummary")) {
-    cli::cli_abort("{.arg x} must be a gtsummary object.", call = get_cli_abort_call())
-  }
   x$table_body <- tail(x$table_body, n = n, ...) # data.frame tail
   x
 }
